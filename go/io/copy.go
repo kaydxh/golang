d179157043abd946a23b64fa72e34de69f09b358
kaydxh <xiao_hua_ding@sina.cn>
2020-11-03 08:31:58 +0800

    [ADD] add copy file

--- go/io/copy.go
+++ go/io/copy.go
@@ -1,6 +1,60 @@
 package io
 
-import "os"
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func CopyFile(src, dst string) (err error) {
+	srcAbs, err := filepath.Abs(src)
+	if err != nil {
+		return err
+	}
+
+	dstAbs, err := filepath.Abs(dst)
+	if err != nil {
+		return err
+	}
+
+	// open source file
+	sfi, err := os.Stat(srcAbs)
+	if err != nil {
+		return err
+	}
+
+	if !sfi.Mode().IsRegular() {
+		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+	}
+
+	// open dest file
+	dfi, err := os.Stat(dstAbs)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return
+		}
+
+		// file doesn't exist
+		err := os.MkdirAll(filepath.Dir(dst), 0755)
+		if err != nil {
+			return err
+		}
+
+	} else {
+		if !(dfi.Mode().IsRegular()) {
+			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
+		}
+		if os.SameFile(sfi, dfi) {
+			return
+		}
+	}
+
+	if err = os.Link(src, dst); err == nil {
+		return
+	}
+	return copyFileContents(src, dst)
+}
 
 //copyFileContentes copies the contents of the file named src to the file named dst
 //The destination file will be created if it does not alreay exist. If the destination
