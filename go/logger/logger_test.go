ec83054c426896ef864a8198ee8fb368dc20bd04
kaydxh <523520531@qq.com>
2021-03-02 15:03:56 +0800

    feat(log): add log unit test

---
+++ go/logger/logger_test.go
@@ -0,0 +1,29 @@
+package logger_test
+
+import (
+	"testing"
+
+	"github.com/kaydxh/golang/go/logger"
+)
+
+/*
+func TestNew(t *testing.T) {
+	l := logger.New(Ctx{"foo": "bar"})
+	if nil == l {
+		t.Error("expect not nil, got nil")
+	}
+}
+*/
+
+func TestInit(t *testing.T) {
+	logger.InitLog(logger.Log_json, logger.Log_info)
+	/*
+		baselog.Level = logrus.PanicLevel
+		baselog.Out = ioutil.Discard
+
+		//	l := logger.NewFromLogger(baselog, Ctx{})
+		assert.NotNil(t, baselog)
+		assert.Equal(t, l.Logger.Level, logrus.PanicLevel)
+		assert.Equal(t, l.Logger.Out, ioutil.Discard)
+	*/
+}
