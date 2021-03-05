f35e4f637ffa0803ea5cad93d646021de2d70f6a
xiaohua ding <xiao_hua_ding@sina.cn>
2021-03-05 11:44:14 +0800

    feat(time ut): unit test for time counter

---
+++ go/time/time_counter_test.go
@@ -0,0 +1,26 @@
+package time_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	time_ "github.com/kaydxh/golang/go/time"
+)
+
+func TestAll(t *testing.T) {
+	var tc time_.TimeCounter
+	tc.Tick("start")
+
+	func(module string) {
+		time.Sleep(time.Second)
+	}("module1")
+	tc.Tick("module1")
+
+	func(module string) {
+		time.Sleep(time.Second * 4)
+	}("module2")
+	tc.Tick("module2")
+
+	fmt.Println(tc.String())
+}
