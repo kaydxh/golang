eaf680409012d418a99c4cf08aa20dc5e03f62db
kaydxh <523520531@qq.com>
2021-03-20 10:51:55 +0800

    feat(time counter): add switch for time counter

--- go/time/time_counter.go
+++ go/time/time_counter.go
@@ -9,14 +9,33 @@ import (
 type TimeCounter struct {
 	starts  []time.Time
 	message []string
+	effect  bool
+}
+
+func New(effect bool) *TimeCounter {
+	t := &TimeCounter{
+		effect: effect,
+	}
+	if effect {
+		t.starts = append(t.starts, time.Now())
+		t.message = append(t.message, "start")
+	}
+
+	return t
 }
 
 func (t *TimeCounter) Tick(msg string) {
-	t.starts = append(t.starts, time.Now())
-	t.message = append(t.message, msg)
+	if t.effect {
+		t.starts = append(t.starts, time.Now())
+		t.message = append(t.message, msg)
+	}
 }
 
 func (t *TimeCounter) String() string {
+	if !t.effect {
+		return ""
+	}
+
 	var buf strings.Builder
 	t.Summary(func(idx int, msg string, cost time.Duration, at time.Time) {
 		buf.WriteString(fmt.Sprintf("#%d, msg: %s, cost: %s, at %s\n", idx, msg, cost, at))
@@ -26,6 +45,9 @@ func (t *TimeCounter) String() string {
 }
 
 func (t *TimeCounter) Summary(f func(idx int, msg string, cost time.Duration, at time.Time)) {
+	if !t.effect {
+		return
+	}
 	if f == nil || t == nil {
 		return
 	}
@@ -37,4 +59,9 @@ func (t *TimeCounter) Summary(f func(idx int, msg string, cost time.Duration, at
 	for i := 1; i < len(t.starts); i++ {
 		f(i, t.message[i], t.starts[i].Sub(t.starts[i-1]), t.starts[i])
 	}
+}
+
+func (t *TimeCounter) Reset() {
+	t.starts = nil
+	t.message = nil
 }
