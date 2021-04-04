3a911917bb07fdc15604dc4982a4e699101ea8f0
kaydxh <523520531@qq.com>
2021-04-04 08:23:53 +0800

    feat(upload): completeMultipartUpload

--- pkg/upload/upload.go
+++ pkg/upload/upload.go
@@ -3,9 +3,11 @@ package upload
 import (
 	"fmt"
 	"mime/multipart"
+	"os"
 	"path/filepath"
 	"strings"
 
+	md5_ "github.com/kaydxh/golang/go/crypto/md5"
 	io_ "github.com/kaydxh/golang/go/io"
 )
 
@@ -14,6 +16,7 @@ type UploadPartInput struct {
 	FileId int64
 	Offset int64
 	Length int64
+	Md5Sum string
 }
 
 func UploadMultipart(
@@ -46,6 +49,37 @@ func UploadMultipart(
 	err = io_.WriteReaderAt(filePath, srcFile, partInput.Offset, partInput.Length)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func CompleteMultipartUpload(
+	filePath string,
+	md5Sum string) error {
+	if filePath == "" {
+		return fmt.Errorf("invalid filePath")
+	}
+
+	if md5Sum != "" {
+		sum, err := md5_.SumFile(filePath)
+		if err != nil {
+			return err
+		}
+
+		gotSum := strings.ToLower(sum)
+		expectSum := strings.ToLower(md5Sum)
+
+		if gotSum != expectSum {
+			return fmt.Errorf("failed to check md5Sum, got: %v, expect: %v", gotSum, expectSum)
+		}
+
+		return nil
+	}
+
+	_, err := os.Stat(filePath)
+	if err != nil {
+		return fmt.Errorf("file: %v is not existed", filePath)
 	}
 
 	return nil
