fa54b30bfa3041142b9b2c9f66107db9dc296f80
kaydxh <523520531@qq.com>
2021-04-09 13:09:41 +0800

    feat(db): rename db

---
+++ pkg/db/rename_db.sql
@@ -0,0 +1,31 @@
+delimiter ;;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `new_db_name` /*!40100 DEFAULT CHARACTER SET utf8*/;
+
+USE `new_db_name`;
+
+SET @queryCount = (
+SELECT COUNT(*) FROM information_schema.TABLES
+WHERE table_schema IN ('old_db_name'));
+
+drop procedure  if exists test_move;
+CREATE PROCEDURE test_move ()
+BEGIN
+
+DECLARE i INT DEFAULT 0;
+WHILE i < @queryCount 
+DO
+	SET @queryString := (
+	SELECT CONCAT('RENAME TABLE ',table_schema,'.',table_name, ' TO ','new_db_name.',table_name,';')
+		FROM information_schema.TABLES WHERE table_schema IN ('old_db_name') ORDER BY table_name LIMIT 1 );
+	PREPARE stmt FROM @queryString;
+	select @queryString as ``;
+	select i as ``;
+	EXECUTE stmt;
+	SET i=i+1;
+END WHILE ;
+commit;
+
+END;;
+delimiter ;
+CALL test_move();
+
