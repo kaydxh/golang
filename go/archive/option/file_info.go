6a79fec39fa90f0a03825c323509e93b13ac875c
xiaohua ding <xiao_hua_ding@sina.cn>
2021-04-18 15:11:43 +0800

    feat(archive): implement String for print

--- go/archive/option/file_info.go
+++ go/archive/option/file_info.go
@@ -1,13 +1,35 @@
 package option
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 type FileInfo struct {
 	Path     string
 	FileInfo os.FileInfo
 }
 
+func (e *FileInfo) String() string {
+	return fmt.Sprintf(
+		"{path: [%v], size: [%v], modeTime: [%v]}",
+		e.Path,
+		e.FileInfo.Size(),
+		e.FileInfo.ModTime(),
+	)
+}
+
 type ExtractMsg struct {
 	FileInfo *FileInfo
 	Error    error
+}
+
+func (e *ExtractMsg) String() string {
+	return fmt.Sprintf(
+		"{path: [%v], size: [%v], modeTime: [%v]}",
+		e.FileInfo.Path,
+		e.FileInfo.FileInfo.Size(),
+		e.FileInfo.FileInfo.ModTime(),
+	)
+
 }
