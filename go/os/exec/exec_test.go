e9c051d4d825262ac7ebe13f1444ee4cc47c546e
kaydxh <523520531@qq.com>
2021-04-22 11:17:13 +0800

    test(os): test for exec timeout

--- go/os/exec/exec_test.go
+++ go/os/exec/exec_test.go
@@ -11,7 +11,17 @@ import (
 func TestExec(t *testing.T) {
 	cmd := fmt.Sprintf(`ps -elf`)
 	timeout := 5000 //ms
-	result, msg, err := exec_.Exec(time.Duration(timeout), "/bin/sh", "-c", cmd)
+	result, msg, err := exec_.Exec(time.Duration(timeout), cmd)
+	if err != nil {
+		t.Errorf("expect nil, got %v, msg: %v", err, msg)
+	}
+	t.Logf("result: %v", result)
+}
+
+func TestExecTimeout(t *testing.T) {
+	cmd := fmt.Sprintf(`sleep 2`)
+	timeout := 1000 //ms
+	result, msg, err := exec_.Exec(time.Duration(timeout), cmd)
 	if err != nil {
 		t.Errorf("expect nil, got %v, msg: %v", err, msg)
 	}
