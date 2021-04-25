34bb773b9d12256aef72de1692f12baaac8d7390
kaydxh <523520531@qq.com>
2021-04-25 16:42:47 +0800

    test(net): test for http

--- go/net/http/client_test.go
+++ go/net/http/client_test.go
@@ -39,3 +39,39 @@ func TestHttpClientGet(t *testing.T) {
 		t.Logf("response data: %v", string(data))
 	}
 }
+
+func TestHttpClientPost(t *testing.T) {
+	client, err := http_.NewClient(http_.WithTimeout(5))
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+
+	testCases := []struct {
+		url      string
+		data     []byte
+		expected bool
+	}{
+		{
+			url:      "http://127.0.0.1",
+			data:     []byte("hello world test1"),
+			expected: true,
+		},
+		{
+			url:      "http://127.0.0.2",
+			data:     []byte("hello world test2"),
+			expected: false,
+		},
+	}
+
+	for _, test := range testCases {
+		data, err := client.Post(test.url, "application/text", test.data)
+		if test.expected {
+			assert.NilError(t, err)
+		} else {
+			assert.Assert(t, err != nil) // NotNil
+			t.Logf("got %v", err)
+		}
+
+		t.Logf("response data: %v", string(data))
+	}
+}
