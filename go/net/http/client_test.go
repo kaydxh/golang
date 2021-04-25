51201e06c05950858c9550cf11a1034dfabe0332
xiaohua ding <xiao_hua_ding@sina.cn>
2021-04-25 15:27:11 +0800

    test(net): test for http

---
+++ go/net/http/client_test.go
@@ -0,0 +1,41 @@
+package http_test
+
+import (
+	"testing"
+
+	http_ "github.com/kaydxh/golang/go/net/http"
+	"gotest.tools/v3/assert"
+)
+
+func TestHttpClientGet(t *testing.T) {
+	client, err := http_.NewClient(http_.WithTimeout(5))
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+
+	testCases := []struct {
+		url      string
+		expected bool
+	}{
+		{
+			url:      "http://127.0.0.1",
+			expected: true,
+		},
+		{
+			url:      "http://127.0.0.2",
+			expected: false,
+		},
+	}
+
+	for _, test := range testCases {
+		resp, err := client.Get(test.url)
+		if test.expected {
+			assert.NilError(t, err)
+		} else {
+			assert.Assert(t, err != nil) // NotNil
+			t.Logf("got %v", err)
+		}
+
+		t.Logf("response: %v", resp)
+	}
+}
