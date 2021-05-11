37d64778060163c83a084b3c9c6bbc048f5621a0
xiaohua ding <xiao_hua_ding@sina.cn>
2021-05-11 14:44:23 +0800

    test(test): test for mount

---
+++ pkg/mount/mount_test.go
@@ -0,0 +1,16 @@
+package mount_test
+
+import (
+	"testing"
+
+	mount_ "github.com/kaydxh/golang/pkg/mount"
+)
+
+func TestMountCeph(t *testing.T) {
+	mountPoint, err := mount_.MountCeph("/mnt", "admin", "127.0.0.1:9090", "password", "/data", false, 30)
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+
+	t.Logf("mountPoint: %v", mountPoint)
+}
