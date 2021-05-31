42bd4567339e33b0837fe7b715f50c33b53a9893
xiaohua ding <xiao_hua_ding@sina.cn>
2021-05-31 15:14:31 +0800

    test(cond): test for BroadCast

--- go/sync/cond_test.go
+++ go/sync/cond_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+	"sync"
+
 	sync_ "github.com/kaydxh/golang/go/sync"
 	"github.com/stretchr/testify/assert"
 )
@@ -18,19 +20,67 @@ func TestSignal(t *testing.T) {
 	go func() {
 		for {
 			time.Sleep(time.Second)
-			cond.SignalDo(func() {
+			cond.SignalDo(func() error {
 				a--
 				fmt.Printf("a: %v\n", a)
+				return nil
 			})
 		}
 	}()
 
-	cond.WaitCond(func() bool {
+	cond.WaitCondDo(func() bool {
 		return a == 3
-	}, func() {
+	}, func() error {
 		a += 100
 		fmt.Printf("a: %v\n", a)
 		assert.Equal(103, a)
+		return nil
 	})
 
+}
+
+func TestBroadCast(t *testing.T) {
+	assert := assert.New(t)
+
+	cond := sync_.NewCond()
+
+	testCases := []struct {
+		threads  int
+		init     int
+		expected int
+	}{
+		{
+			threads:  5,
+			init:     10,
+			expected: 3,
+		},
+	}
+
+	var wg sync.WaitGroup
+
+	for i, testCase := range testCases {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			cond.WaitCondDo(func() bool {
+				return testCase.expected == testCases[i].init
+			}, func() error {
+				assert.Equal(testCases[i].init, testCase.expected)
+				return nil
+			})
+		}()
+	}
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			cond.BroadcastDo(func() error {
+				testCases[0].init--
+				fmt.Printf("init: %v\n", testCases[0].init)
+				return nil
+			})
+		}
+	}()
+
+	wg.Wait()
 }
