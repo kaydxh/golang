b1b8a70457116becf726ba2c9585700022aca455
xiaohua ding <xiao_hua_ding@sina.cn>
2021-06-10 09:19:13 +0800

    feat(time): add time default format

--- go/time/time.go
+++ go/time/time.go
@@ -2,6 +2,12 @@ package time
 
 import "time"
 
+const (
+	DayFormat           = "20060102"
+	DefaultTimeFormat   = "2006-01-02 15:04:05"
+	DefaultTimeMsFormat = "2006-01-02 15:04:05.000"
+)
+
 func BeginningOfDay(days int) time.Time {
 	now := time.Now()
 	theDay := now.AddDate(0, 0, days)
@@ -22,7 +28,7 @@ func BeginningOfDayString(days int, layout string) string {
 	y, m, d := theDay.Date()
 
 	if layout == "" {
-		layout = "2006-01-02 15:04:05"
+		layout = DefaultTimeFormat
 	}
 	return time.Date(y, m, d, 0, 0, 0, 0, theDay.Location()).Format(layout)
 }
@@ -33,7 +39,7 @@ func EndOfDayString(days int, layout string) string {
 	y, m, d := theDay.Date()
 
 	if layout == "" {
-		layout = "2006-01-02 15:04:05"
+		layout = DefaultTimeFormat
 	}
 	return time.Date(
 		y,
