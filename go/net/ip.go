775458aff13867f4786b3c47c513bbb24e002e11
kaydxh <523520531@qq.com>
2021-07-05 16:02:02 +0800

    feat(net): get local ips

---
+++ go/net/ip.go
@@ -0,0 +1,55 @@
+package net
+
+import (
+	"fmt"
+	"net"
+)
+
+func GetLocalFirstIP() (string, error) {
+	ips, err := GetLocalIPs()
+	if err != nil {
+		return "", err
+	}
+	if len(ips) == 0 {
+		return "", fmt.Errorf("no invalid ip")
+	}
+
+	return ips[0], nil
+}
+
+func GetLocalIPs() ([]string, error) {
+	localAddrs, err := GetLocalAddrs()
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for _, addr := range localAddrs {
+		if addr.IP.To4() != nil && !addr.IP.IsLoopback() {
+			ips = append(ips, addr.IP.String())
+		}
+
+	}
+
+	return ips, nil
+}
+
+func GetLocalAddrs() ([]*net.IPNet, error) {
+	var localAddrs []*net.IPNet
+
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, addr := range addrs {
+		_, ipNet, err := net.ParseCIDR(addr.String())
+		if err != nil {
+			return nil, err
+		}
+
+		localAddrs = append(localAddrs, ipNet)
+	}
+
+	return localAddrs, nil
+}
