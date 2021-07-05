f1ffe4ad2217f24bbe05f6d45e0099b95804f350
kayxhding <kayxhding@tencent.com>
2021-07-05 16:36:52 +0800

    style(net): update error message

--- go/net/ip_test.go
+++ go/net/ip_test.go
@@ -9,7 +9,7 @@ import (
 func TestGetLocalAddrs(t *testing.T) {
 	addrs, err := net_.GetLocalAddrs()
 	if err != nil {
-		t.Fatalf("failed to get local adddrs, err: %v", err)
+		t.Fatalf("failed to get local addrs, err: %v", err)
 		return
 	}
 	t.Logf("addrs: %v", addrs)
@@ -18,7 +18,7 @@ func TestGetLocalAddrs(t *testing.T) {
 func TestGetLocalIPs(t *testing.T) {
 	ips, err := net_.GetLocalIPs()
 	if err != nil {
-		t.Fatalf("failed to get local adddrs, err: %v", err)
+		t.Fatalf("failed to get local addrs, err: %v", err)
 		return
 	}
 	t.Logf("ips: %v", ips)
@@ -27,7 +27,7 @@ func TestGetLocalIPs(t *testing.T) {
 func TestGetLocalFirstIP(t *testing.T) {
 	ip, err := net_.GetLocalFirstIP()
 	if err != nil {
-		t.Fatalf("failed to get local first adddr, err: %v", err)
+		t.Fatalf("failed to get local first addr, err: %v", err)
 		return
 	}
 	t.Logf("ip: %v", ip)
