79849c705f20050d498e69100db181d19a8128e1
kaydxh <523520531@qq.com>
2021-07-05 16:02:59 +0800

    test(net): test for  getting local ips

---
+++ go/net/ip_test.go
@@ -0,0 +1,34 @@
+package net_test
+
+import (
+	"testing"
+
+	net_ "github.com/kaydxh/golang/go/net"
+)
+
+func TestGetLocalAddrs(t *testing.T) {
+	addrs, err := net_.GetLocalAddrs()
+	if err != nil {
+		t.Fatalf("failed to get local adddrs, err: %v", err)
+		return
+	}
+	t.Logf("addrs: %v", addrs)
+}
+
+func TestGetLocalIPs(t *testing.T) {
+	ips, err := net_.GetLocalIPs()
+	if err != nil {
+		t.Fatalf("failed to get local adddrs, err: %v", err)
+		return
+	}
+	t.Logf("ips: %v", ips)
+}
+
+func TestGetLocalFirstIP(t *testing.T) {
+	ip, err := net_.GetLocalFirstIP()
+	if err != nil {
+		t.Fatalf("failed to get local first adddr, err: %v", err)
+		return
+	}
+	t.Logf("ip: %v", ip)
+}
