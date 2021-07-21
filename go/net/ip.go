e4e20a33bb092f869bd2b079945155cef9d0f148
kaydxh <523520531@qq.com>
2021-07-21 15:20:05 +0800

    feat(net): get host ip by score

--- go/net/ip.go
+++ go/net/ip.go
@@ -1,9 +1,76 @@
 package net
 
 import (
+	"errors"
 	"fmt"
 	"net"
 )
+
+// This code is borrowed from https://github.com/uber/tchannel-go/blob/dev/localip.go
+
+// scoreAddr scores how likely the given addr is to be a remote address and returns the
+// IP to use when listening. Any address which receives a negative score should not be used.
+// Scores are calculated as:
+// -1 for any unknown IP addresses.
+// +300 for IPv4 addresses
+// +100 for non-local addresses, extra +100 for "up" interaces.
+func scoreAddr(iface net.Interface, addr net.Addr) (int, net.IP) {
+	var ip net.IP
+	if netAddr, ok := addr.(*net.IPNet); ok {
+		ip = netAddr.IP
+	} else if netIP, ok := addr.(*net.IPAddr); ok {
+		ip = netIP.IP
+	} else {
+		return -1, nil
+	}
+
+	var score int
+	if ip.To4() != nil {
+		score += 300
+	}
+	if iface.Flags&net.FlagLoopback == 0 && !ip.IsLoopback() {
+		score += 100
+		if iface.Flags&net.FlagUp != 0 {
+			score += 100
+		}
+	}
+
+	fmt.Printf("ip: %v, score: %v\n", ip, score)
+	return score, ip
+}
+
+// HostIP tries to find an IP that can be used by other machines to reach this machine.
+func GetHostIP() (net.IP, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	bestScore := -1
+	var bestIP net.IP
+	// Select the highest scoring IP as the best IP.
+	for _, iface := range interfaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			// Skip this interface if there is an error.
+			continue
+		}
+
+		for _, addr := range addrs {
+			score, ip := scoreAddr(iface, addr)
+			if score > bestScore {
+				bestScore = score
+				bestIP = ip
+			}
+		}
+	}
+
+	if bestScore == -1 {
+		return nil, errors.New("no addresses to listen on")
+	}
+
+	return bestIP, nil
+}
 
 func GetLocalFirstIP() (string, error) {
 	ips, err := GetLocalIPs()
@@ -36,19 +103,16 @@ func GetLocalIPs() ([]string, error) {
 
 func GetLocalAddrs() ([]*net.IPNet, error) {
 	var localAddrs []*net.IPNet
-
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		return nil, err
 	}
 
 	for _, addr := range addrs {
-		_, ipNet, err := net.ParseCIDR(addr.String())
-		if err != nil {
-			return nil, err
+		ipNet, ok := addr.(*net.IPNet)
+		if ok {
+			localAddrs = append(localAddrs, ipNet)
 		}
-
-		localAddrs = append(localAddrs, ipNet)
 	}
 
 	return localAddrs, nil
