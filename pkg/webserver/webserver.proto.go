0122e2633b929efef06cd61dbd1fdef6b14d783b
kaydxh <523520531@qq.com>
2021-07-21 19:44:16 +0800

    fix(webserver): fix get host and ip

--- pkg/webserver/webserver.proto.go
+++ pkg/webserver/webserver.proto.go
@@ -5,5 +5,5 @@ import "fmt"
 func (w *Web) GetBindHostPort() string {
 	addr := w.GetBindAddress()
 
-	return fmt.Sprintf("%s:%d:", addr.GetHost(), addr.GetPort())
+	return fmt.Sprintf("%s:%d", addr.GetHost(), addr.GetPort())
 }
