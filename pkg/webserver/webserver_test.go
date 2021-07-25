42781650b9e2a8ad670dfa220324d8a87801cac8
kaydxh <523520531@qq.com>
2021-07-25 13:18:14 +0800

    feat(webserver): get webserver config from viper, which new with yaml file from external project

--- pkg/webserver/webserver_test.go
+++ pkg/webserver/webserver_test.go
@@ -1,15 +1,35 @@
 package webserver_test
 
 import (
+	"fmt"
 	"testing"
 
 	"context"
 
 	webserver_ "github.com/kaydxh/golang/pkg/webserver"
+	"github.com/ory/viper"
 )
 
 func TestNew(t *testing.T) {
-	config := new(webserver_.Config)
+	/*
+		viper.SetConfigName("webserver")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath(".")
+	*/
+	viper.SetConfigFile("./webserver.yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		t.Errorf("failed to read config err: %v", err)
+		return
+	}
+	subv := viper.Sub("web")
+	config := webserver_.NewConfig(webserver_.WithGetViper(func() *viper.Viper {
+		return subv //viper.GetViper()
+	}))
+
+	//fmt.Println(viper.GetViper())
+	fmt.Println(subv)
+
 	s, err := config.Complete().New()
 	if err != nil {
 		t.Errorf("failed to new config err: %v", err)
