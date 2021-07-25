ecdc0bc2c835e17e8bd054f2f054c9f72f589edb
kaydxh <523520531@qq.com>
2021-07-25 19:19:18 +0800

    feat(viper): add get viper with config file and subkey

--- pkg/viper/viper.go
+++ pkg/viper/viper.go
@@ -1,1 +1,31 @@
 package viper
+
+import (
+	"fmt"
+
+	"github.com/ory/viper"
+)
+
+func GetViper(configFile string, subKey string) (*viper.Viper, error) {
+
+	if configFile == "" {
+		v := viper.GetViper()
+		if v == nil {
+			return nil, fmt.Errorf("failed to get viper without config file")
+		}
+
+		return v, nil
+	}
+
+	viper.SetConfigFile(configFile)
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config err: %v", err)
+	}
+
+	if subKey == "" {
+		return viper.GetViper(), nil
+	}
+
+	return viper.Sub(subKey), nil
+}
