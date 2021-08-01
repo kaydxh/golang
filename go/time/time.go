fb4c808308639ffb85b81b21c2707c6927361e73
kayxhding <kayxhding@tencent.com>
2021-08-01 18:21:25 +0800

    feat(time): truncate to utc time with duration

--- go/time/time.go
+++ go/time/time.go
@@ -66,3 +66,43 @@ func EndOfDayString(days int, layout string) string {
 		theDay.Location(),
 	).Format(layout)
 }
+
+// Truncate only happens in UTC semantics, apparently.
+// observed values for truncating given time with 86400 secs:
+//
+// before truncation: 2018/06/01 03:54:54 2018-06-01T03:18:00+09:00
+// after  truncation: 2018/06/01 03:54:54 2018-05-31T09:00:00+09:00
+//
+// This is really annoying when we want to truncate in local time
+// so we hack: we take the apparent local time in the local zone,
+// and pretend that it's in UTC. do our math, and put it back to
+// the local zone
+func TruncateToUTC(t time.Time, d time.Duration) time.Time {
+	if t.Location() == time.UTC {
+		return t.Truncate(d)
+	}
+
+	base := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.UTC)
+	base = base.Truncate(d)
+
+	return time.Date(
+		base.Year(),
+		base.Month(),
+		base.Day(),
+		base.Hour(),
+		base.Minute(),
+		base.Second(),
+		base.Nanosecond(),
+		base.Location(),
+	)
+}
+
+func TruncateToUTCString(t time.Time, d time.Duration, layout string) string {
+	utc := TruncateToUTC(t, d)
+
+	if layout == "" {
+		layout = DefaultTimeFormat
+	}
+
+	return utc.Format(layout)
+}
