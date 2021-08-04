f9588e69c2692dd009662b3693b652736a10276d
kayxhding <kayxhding@tencent.com>
2021-08-04 08:42:08 +0800

    feat(logs): install log with rotate

---
+++ pkg/logs/log.option.go
@@ -0,0 +1,33 @@
+package logs
+
+import "time"
+
+func WithPrefixName(prefixName string) RotateOption {
+	return RotateOptionFunc(func(c *Rotate) {
+		c.prefixName = prefixName
+	})
+}
+
+func WithSuffixName(suffixName string) RotateOption {
+	return RotateOptionFunc(func(c *Rotate) {
+		c.suffixName = suffixName
+	})
+}
+
+func WithMaxAge(maxAge time.Duration) RotateOption {
+	return RotateOptionFunc(func(c *Rotate) {
+		c.maxAge = maxAge
+	})
+}
+
+func WithRotateSize(rotateSize int64) RotateOption {
+	return RotateOptionFunc(func(c *Rotate) {
+		c.rotateSize = rotateSize
+	})
+}
+
+func WithRotateInterval(rotateInterval time.Duration) RotateOption {
+	return RotateOptionFunc(func(c *Rotate) {
+		c.rotateInterval = rotateInterval
+	})
+}
