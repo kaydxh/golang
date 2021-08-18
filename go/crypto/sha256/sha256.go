037a3b0168a2025d93cf734f7a6f5b6f68ceb010
kayxhding <kayxhding@tencent.com>
2021-08-18 17:57:52 +0800

    feat(crypto): sha256

---
+++ go/crypto/sha256/sha256.go
@@ -0,0 +1,89 @@
+package sha256
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"os"
+
+	os_ "github.com/kaydxh/golang/go/os"
+)
+
+func SumBytes(b []byte) string {
+	h := sha256.New()
+	h.Write(b)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func SumString(s string) string {
+	return SumBytes([]byte(s))
+}
+
+func SumReader(r io.Reader) (string, error) {
+	h := sha256.New()
+	if _, err := io.Copy(h, r); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func SumReaderN(r io.Reader, n int64) (string, error) {
+	h := sha256.New()
+	if _, err := io.CopyN(h, r, n); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func SumReaderAt(r io.ReaderAt, offset, length int64) (string, error) {
+	h := sha256.New()
+	buf := make([]byte, 1024)
+	var total int64
+
+	for total < length {
+		n, err := r.ReadAt(buf, offset)
+		if err == nil || err == io.EOF {
+			if n > 0 {
+				_, tmpErr := io.CopyN(h, bytes.NewReader(buf), int64(n))
+				if tmpErr != nil {
+					return "", tmpErr
+				}
+				offset += int64(n)
+				total += int64(n)
+
+			}
+
+			if err == io.EOF {
+				break
+			}
+		} else {
+			return "", err
+		}
+
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func SumFile(fileName string) (string, error) {
+	file, err := os_.OpenFile(fileName, true)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	return SumReader(file)
+}
+
+func SumFileAt(fileName string, offset int64, length int64) (string, error) {
+	file, err := os_.OpenAll(fileName, os.O_RDWR, os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	return SumReaderAt(file, offset, length)
+}
---
+++ go/crypto/sha256/sha256_test.go
@@ -0,0 +1,85 @@
+package sha256_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	sha256_ "github.com/kaydxh/golang/go/crypto/sha256"
+	"gotest.tools/assert"
+)
+
+func TestSha256File(t *testing.T) {
+	file, err := ioutil.TempFile(".", "file")
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	defer os.RemoveAll(file.Name())
+
+	strContext := "hello world"
+	//	n, err := io.WriteString(file, "hello world")
+	n, err := file.Write([]byte(strContext))
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	assert.Equal(t, len(strContext), n)
+	fmt.Printf("fileName: %v, n: %v\n", file.Name(), n)
+
+	sum, err := sha256_.SumFile(file.Name())
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	assert.Equal(t, sum, sha256_.SumString(strContext))
+}
+
+func TestSha256FileAt(t *testing.T) {
+	file, err := ioutil.TempFile(".", "file")
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	defer os.RemoveAll(file.Name())
+
+	testCases := []struct {
+		name     string
+		words    []byte
+		expected string
+	}{
+		{
+			name:     "write one word",
+			words:    []byte("test1"),
+			expected: "test1",
+		},
+		{
+			name:     "write one word",
+			words:    []byte("test2"),
+			expected: "test2",
+		},
+
+		{
+			name:     "write one word",
+			words:    []byte("test3"),
+			expected: "test3",
+		},
+	}
+
+	var offset int64
+	for i, testCase := range testCases {
+		_, err := file.Write(testCase.words)
+		if err != nil {
+			t.Errorf("expect nil, got %v", err)
+		}
+
+		if i > 0 {
+			offset += int64(len(testCases[i-1].words))
+		}
+		fmt.Printf("i: %d, offset: %v, testCase: %s\n", i, offset, testCase.words)
+		sum, err := sha256_.SumFileAt(file.Name(), offset, int64(len(testCase.words)))
+		if err != nil {
+			t.Errorf("expect nil, got %v", err)
+		}
+		fmt.Println("sum: ", sum)
+		assert.Equal(t, sum, sha256_.SumBytes(testCase.words))
+	}
+
+}
