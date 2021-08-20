45bb6ad511982fed5b4ae260f66b8f805d858858
kaydxh <523520531@qq.com>
2021-08-20 11:07:49 +0800

    feat(grpc middleware): http error for api 3.0

--- pkg/grpc-gateway/grpc_gateway.option.go
+++ pkg/grpc-gateway/grpc_gateway.option.go
@@ -2,21 +2,28 @@ package grpcgateway
 
 import (
 	interceptorlogrus_ "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	interceptortcloud_ "github.com/kaydxh/golang/pkg/grpc-middleware/api/tcloud/v3.0"
 	interceptortimer_ "github.com/kaydxh/golang/pkg/grpc-middleware/timer"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
-func WithServerOptions(opts []grpc.ServerOption) GRPCGatewayOption {
+func WithServerOptions(opts ...grpc.ServerOption) GRPCGatewayOption {
 	return GRPCGatewayOptionFunc(func(c *GRPCGateway) {
-		c.opts.serverOptions = opts
+		c.opts.serverOptions = append(c.opts.serverOptions, opts...)
 	})
 }
 
-func WithClientDialOptions(opts []grpc.DialOption) GRPCGatewayOption {
+func WithClientDialOptions(opts ...grpc.DialOption) GRPCGatewayOption {
 	return GRPCGatewayOptionFunc(func(c *GRPCGateway) {
-		c.opts.clientDialOptions = opts
+		c.opts.clientDialOptions = append(c.opts.clientDialOptions, opts...)
+	})
+}
+
+func WithGatewayMuxOptions(opts ...runtime.ServeMuxOption) GRPCGatewayOption {
+	return GRPCGatewayOptionFunc(func(c *GRPCGateway) {
+		c.opts.gatewayMuxOptions = append(c.opts.gatewayMuxOptions, opts...)
 	})
 }
 
@@ -64,5 +71,11 @@ func WithServerUnaryInterceptorsErrorOptions() GRPCGatewayOption {
 	return GRPCGatewayOptionFunc(func(c *GRPCGateway) {
 		WithServerUnaryInterceptorsOptions(interceptortcloud_.UnaryServerInterceptorOfError()).apply(c)
 		//		WithServerStreamInterceptorsOptions(interceptorlogrus_.StreamServerInterceptor(l))
+	})
+}
+
+func WithServerInterceptorsHttpErrorOptions() GRPCGatewayOption {
+	return GRPCGatewayOptionFunc(func(c *GRPCGateway) {
+		WithGatewayMuxOptions(runtime.WithErrorHandler(interceptortcloud_.HTTPError)).apply(c)
 	})
 }
