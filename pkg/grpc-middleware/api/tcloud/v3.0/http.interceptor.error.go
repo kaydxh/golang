18d004426d16f11e33e7f8c1361ddde8f19c75c1
kayxhding <kayxhding@tencent.com>
2021-08-20 15:25:03 +0800

    feat(grpc middleware): get requestId  from metaData

--- pkg/grpc-middleware/api/tcloud/v3.0/http.interceptor.error.go
+++ pkg/grpc-middleware/api/tcloud/v3.0/http.interceptor.error.go
@@ -2,22 +2,21 @@ package tcloud
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	strings_ "github.com/kaydxh/golang/go/strings"
 )
 
 // HTTPError uses the mux-configured error handler.
 func HTTPError(ctx context.Context, mux *runtime.ServeMux,
 	marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
-	fmt.Printf("err: %v", err)
 	errResponse := &ErrorResponse{
 		Error: &TCloudError{
 			Code:    errorToCode(err).String(),
 			Message: errorToString(err),
 		},
-		RequestId: retrieveRequestId(r),
+		RequestId: strings_.GetStringOrFallback(GetMetadata(ctx, RequestIdKey), ""),
 	}
 	// ForwardResponseMessage forwards the message "resp" from gRPC server to REST client
 	runtime.ForwardResponseMessage(ctx, mux, marshaler, w, r, errResponse)
