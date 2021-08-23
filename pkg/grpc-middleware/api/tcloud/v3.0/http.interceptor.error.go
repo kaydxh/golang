9fa396e490fc189395ac6801046e5bdb99d5d544
kaydxh <523520531@qq.com>
2021-08-23 14:22:24 +0800

    refactor(interceptor): jsonpb to proto message

--- pkg/grpc-middleware/api/tcloud/v3.0/http.interceptor.error.go
+++ pkg/grpc-middleware/api/tcloud/v3.0/http.interceptor.error.go
@@ -2,15 +2,20 @@ package tcloud
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	strings_ "github.com/kaydxh/golang/go/strings"
+	jsonpb_ "github.com/kaydxh/golang/pkg/protobuf/jsonpb"
+	"google.golang.org/protobuf/proto"
 )
 
 // HTTPError uses the mux-configured error handler.
 func HTTPError(ctx context.Context, mux *runtime.ServeMux,
 	marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+
 	errResponse := &ErrorResponse{
 		Error: &TCloudError{
 			Code:    errorToCode(err).String(),
@@ -18,6 +23,27 @@ func HTTPError(ctx context.Context, mux *runtime.ServeMux,
 		},
 		RequestId: strings_.GetStringOrFallback(GetMetadata(ctx, RequestIdKey), ""),
 	}
+
 	// ForwardResponseMessage forwards the message "resp" from gRPC server to REST client
 	runtime.ForwardResponseMessage(ctx, mux, marshaler, w, r, errResponse)
+}
+
+//cant not rewrite message, only append message to response
+func HTTPForwardResponse(ctx context.Context, r http.ResponseWriter, message proto.Message) error {
+	respStruct, err := jsonpb_.MarshaToStructpb(message)
+	if err != nil {
+		return err
+	}
+	errResponse := &TCloudResponse{
+		Response: respStruct,
+	}
+
+	jb, err := json.Marshal(errResponse)
+	if err != nil {
+		return fmt.Errorf("jsonpb.Marshal: %v", err)
+	}
+
+	r.Write(jb)
+
+	return nil
 }
---
+++ pkg/protobuf/jsonpb/jsonpb_marshal.go
@@ -0,0 +1,51 @@
+package jsonpb
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+	structpb "github.com/golang/protobuf/ptypes/struct"
+)
+
+func MarshaToStructpb(v interface{}) (*structpb.Struct, error) {
+	var jb []byte
+	switch v := v.(type) {
+	case nil:
+		return &structpb.Struct{}, nil
+
+	case *structpb.Struct:
+		return v, nil
+
+	case proto.Message:
+		m := jsonpb.Marshaler{EmitDefaults: true}
+		data, err := m.MarshalToString(v)
+		if err != nil {
+			return nil, fmt.Errorf("jsonpb.Marshal: %v", err)
+		}
+		jb = []byte(data)
+
+	case []byte:
+		jb = v
+	case *[]byte:
+		jb = *v
+	case string:
+		jb = []byte(v)
+	case *string:
+		jb = []byte(*v)
+	default:
+		var err error
+		jb, err = json.Marshal(v)
+		if err != nil {
+			return nil, fmt.Errorf("jsonpb.Marshal: %v", err)
+		}
+	}
+
+	var dataStructpb structpb.Struct
+	if err := UnmarshalWithAllowUnknownFields(bytes.NewReader(jb), &dataStructpb); err != nil {
+		return nil, err
+	}
+	return &dataStructpb, nil
+}
