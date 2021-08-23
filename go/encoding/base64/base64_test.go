1fed9218553c1ea6313fd2ca418b01550c07c1cb
kaydxh <523520531@qq.com>
2021-08-23 14:23:19 +0800

    test(base64): test base64 for multi cases

--- go/encoding/base64/base64_test.go
+++ go/encoding/base64/base64_test.go
@@ -8,15 +8,43 @@ import (
 )
 
 func TestString(t *testing.T) {
-	content := "hello word"
-
-	encoded := base64_.EncodeString(content)
-	t.Logf("base64 encode : %v", encoded)
-
-	decoded, err := base64_.DecodeString(encoded)
-	if err != nil {
-		t.Fatalf("failed to decode string, err: %v", err)
+	testCases := []struct {
+		name     string
+		expected string
+	}{
+		{
+			name:     "hello word",
+			expected: "",
+		},
+		{
+			name:     "http://12306.com?a=%b",
+			expected: "",
+		},
 	}
-	t.Logf("base64 decode : %v", decoded)
-	assert.Equal(t, content, decoded)
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			encoded := base64_.EncodeString(testCase.name)
+			t.Logf("base64 encode : %v", encoded)
+			decoded, err := base64_.DecodeString(encoded)
+			if err != nil {
+				t.Fatalf("failed to decode string, err: %v", err)
+			}
+
+			assert.Equal(t, testCase.name, decoded)
+		})
+	}
+	/*
+		content := "hello word"
+
+		encoded := base64_.EncodeString(content)
+		t.Logf("base64 encode : %v", encoded)
+
+		decoded, err := base64_.DecodeString(encoded)
+		if err != nil {
+			t.Fatalf("failed to decode string, err: %v", err)
+		}
+		t.Logf("base64 decode : %v", decoded)
+	*/
+	//assert.Equal(t, content, decoded)
 }
