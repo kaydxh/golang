f17c025415fa7f236b3c0627cef3091ccf9532e6
kaydxh <523520531@qq.com>
2021-08-23 13:02:23 +0800

    feat(jsonpb): add optinos for new jsonpb

---
+++ pkg/grpc-middleware/api/tcloud/v3.0/jsonpb.marshale_option.go
@@ -0,0 +1,38 @@
+package tcloud
+
+// A JSONPbOption sets options.
+type JSONPbOption interface {
+	apply(*JSONPb)
+}
+
+// EmptyJSONPbOption does not alter the configuration. It can be embedded
+// in another structure to build custom options.
+//
+// This API is EXPERIMENTAL.
+type EmptyJSONPbOption struct{}
+
+func (EmptyJSONPbOption) apply(*JSONPb) {}
+
+// JSONPbOptionFunc wraps a function that modifies Client into an
+// implementation of the JSONPbOption interface.
+type JSONPbOptionFunc func(*JSONPb)
+
+func (f JSONPbOptionFunc) apply(do *JSONPb) {
+	f(do)
+}
+
+// sample code for option, default for nothing to change
+func _JSONPbOptionWithDefault() JSONPbOption {
+	return JSONPbOptionFunc(func(*JSONPb) {
+		// nothing to change
+	})
+}
+func (o *JSONPb) ApplyOptions(options ...JSONPbOption) *JSONPb {
+	for _, opt := range options {
+		if opt == nil {
+			continue
+		}
+		opt.apply(o)
+	}
+	return o
+}
