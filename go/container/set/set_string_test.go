3143a29f1d140f87a3c32cd5955594dcbbee1140
kayxhding <kayxhding@tencent.com>
2021-08-24 17:14:00 +0800

    test(container): test for string set

--- go/container/set/set_string_test.go
+++ go/container/set/set_string_test.go
@@ -49,3 +49,98 @@ func TestStringSetEquals(t *testing.T) {
 		t.Errorf("Expected to be equal: %v vs %v", a, b)
 	}
 }
+
+func TestStringSetUnion(t *testing.T) {
+	tests := []struct {
+		s1       set_.String
+		s2       set_.String
+		expected set_.String
+	}{
+		{
+			set_.NewString("1", "2", "3", "4"),
+			set_.NewString("3", "4", "5", "6"),
+			set_.NewString("1", "2", "3", "4", "5", "6"),
+		},
+		{
+			set_.NewString("1", "2", "3", "4"),
+			set_.NewString(),
+			set_.NewString("1", "2", "3", "4"),
+		},
+		{
+			set_.NewString(),
+			set_.NewString("1", "2", "3", "4"),
+			set_.NewString("1", "2", "3", "4"),
+		},
+		{
+			set_.NewString(),
+			set_.NewString(),
+			set_.NewString(),
+		},
+	}
+
+	for _, test := range tests {
+		union := test.s1.Union(test.s2)
+		if union.Len() != test.expected.Len() {
+			t.Errorf("Expected union.Len()=%d but got %d", test.expected.Len(), union.Len())
+		}
+
+		if !union.Equal(test.expected) {
+			t.Errorf(
+				"Expected union.Equal(expected) but not true.  union:%v expected:%v",
+				union.List(),
+				test.expected.List(),
+			)
+		}
+	}
+
+}
+
+func TestStringSetIntersection(t *testing.T) {
+	tests := []struct {
+		s1       set_.String
+		s2       set_.String
+		expected set_.String
+	}{
+		{
+			set_.NewString("1", "2", "3", "4"),
+			set_.NewString("3", "4", "5", "6"),
+			set_.NewString("3", "4"),
+		},
+		{
+			set_.NewString("1", "2", "3", "4"),
+			set_.NewString("1", "2", "3", "4"),
+			set_.NewString("1", "2", "3", "4"),
+		},
+		{
+			set_.NewString("1", "2", "3", "4"),
+			set_.NewString(),
+			set_.NewString(),
+		},
+		{
+			set_.NewString(),
+			set_.NewString("1", "2", "3", "4"),
+			set_.NewString(),
+		},
+		{
+			set_.NewString(),
+			set_.NewString(),
+			set_.NewString(),
+		},
+	}
+
+	for _, test := range tests {
+		intersection := test.s1.Intersection(test.s2)
+		if intersection.Len() != test.expected.Len() {
+			t.Errorf("Expected intersection.Len()=%d but got %d", test.expected.Len(), intersection.Len())
+		}
+
+		if !intersection.Equal(test.expected) {
+			t.Errorf(
+				"Expected intersection.Equal(expected) but not true.  intersection:%v expected:%v",
+				intersection.List(),
+				test.expected.List(),
+			)
+		}
+	}
+
+}
