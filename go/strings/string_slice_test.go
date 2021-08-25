85fd025797b9a068a5dd8e08f915743335278ee7
kayxhding <kayxhding@tencent.com>
2021-08-25 15:21:20 +0800

    feat(strings): add slice intersection

---
+++ go/strings/string_slice_test.go
@@ -0,0 +1,34 @@
+package strings_test
+
+import (
+	"testing"
+
+	strings_ "github.com/kaydxh/golang/go/strings"
+)
+
+func TestSetUnion(t *testing.T) {
+	testCases := []struct {
+		name     string
+		s1       []string
+		s2       []string
+		expected []string
+	}{
+		{
+			name:     "test string",
+			s1:       []string{"1", "2", "3", "4"},
+			s2:       []string{"3", "4", "5", "6"},
+			expected: []string{"3", "4"},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			intersection := strings_.SliceIntersection(testCase.s1, testCase.s2)
+			if len(intersection) != len(testCase.expected) {
+				t.Fatalf("Expected Intersection len: %v, got : %v", len(testCase.expected), len(intersection))
+
+			}
+
+		})
+	}
+}
