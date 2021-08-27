00a47a5e3df6b7ea3370ee3037e2f4b3d83f817f
kaydxh <523520531@qq.com>
2021-08-27 15:44:23 +0800

    feat(proxy): add target url and match router variable

--- pkg/proxy/proxy.option.go
+++ pkg/proxy/proxy.option.go
@@ -5,3 +5,15 @@ func WithRouterPatterns(routerPatterns ...string) ReverseProxyOption {
 		c.opts.routerPatterns = append(c.opts.routerPatterns, routerPatterns...)
 	})
 }
+
+func WithTargetUrl(targetUrl string) ReverseProxyOption {
+	return ReverseProxyOptionFunc(func(c *ReverseProxy) {
+		c.opts.targetUrl = targetUrl
+	})
+}
+
+func WithMatchRouterFunc(matchRouter MatchRouterFunc) ReverseProxyOption {
+	return ReverseProxyOptionFunc(func(c *ReverseProxy) {
+		c.opts.matchRouter = matchRouter
+	})
+}
