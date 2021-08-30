b157921a71d8d3b45499929d3b070dfb677b8da5
kaydxh <523520531@qq.com>
2021-08-30 11:26:58 +0800

    feat(base64): base64 for url

--- go/encoding/base64/base64.go
+++ go/encoding/base64/base64.go
@@ -17,3 +17,16 @@ func DecodeString(v string) (string, error) {
 
 	return string(data), nil
 }
+
+func EncodeURL(v string) string {
+	return base64.URLEncoding.EncodeToString([]byte(v))
+}
+
+func DecodeURL(v string) (string, error) {
+	data, err := base64.URLEncoding.DecodeString(v)
+	if err != nil {
+		return "", fmt.Errorf("base64 decode url failed: %v", err)
+	}
+
+	return string(data), nil
+}
