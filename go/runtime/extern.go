4ae27eb5842ce9da80d802f70c3fbbf5f13ad324
kaydxh <523520531@qq.com>
2021-09-11 20:28:44 +0800

    feat(runtime): get caller

--- go/runtime/extern.go
+++ go/runtime/extern.go
@@ -7,9 +7,9 @@ import (
 
 // GetCaller returns the caller of the function that calls it.
 //The argument skip is the number of stack frames to skip before recording in pc, with 0 identifying the frame for Callers itself and 1 identifying the caller of Callers
-func GetCaller(skip int) string {
+func GetCallerWithSkip(skip int) string {
 	var pc [1]uintptr
-	runtime.Callers(skip, pc[:])
+	runtime.Callers(skip+1, pc[:])
 	f := runtime.FuncForPC(pc[0])
 	if f == nil {
 		return fmt.Sprintf("Unable to find caller")
@@ -17,6 +17,7 @@ func GetCaller(skip int) string {
 	return f.Name()
 }
 
-func GetParentCaller() string {
-	return GetCaller(1)
+func GetCaller() string {
+	//4 skip, 1 GetCaller, 1 GetCallerWithSkip, 1 runtime.Callers, 1 caller of  GetCaller
+	return GetCallerWithSkip(3)
 }
