03d0e461cabd308c90feec0a568b800dfa08e23e
kaydxh <523520531@qq.com>
2021-09-25 17:38:11 +0800

    style(redis): add detail err for hset

--- pkg/database/redis/command.hset.go
+++ pkg/database/redis/command.hset.go
@@ -9,6 +9,7 @@ import (
 )
 
 // only get export Fields from arg
+// set tag-values for field-value to hash
 func HSetStruct(ctx context.Context, db *redis.Client, key string, arg interface{}) error {
 
 	if db == nil {
@@ -16,7 +17,10 @@ func HSetStruct(ctx context.Context, db *redis.Client, key string, arg interface
 	}
 
 	tagsValues := reflect_.AllTagsValues(arg, "db")
-	_, err := db.HMSet(ctx, key, tagsValues).Result()
+	_, err := db.HSet(ctx, key, tagsValues).Result()
+	if err != nil {
+		return fmt.Errorf("failed to HSet key: %v, err: %v", key, err)
+	}
 
-	return err
+	return nil
 }
