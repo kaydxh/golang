b9e1d78f44245b1364a97a458e4d299bb8e0fa77
kaydxh <523520531@qq.com>
2021-09-30 15:44:33 +0800

    feat(grpc-middleware): add UnaryServerInterceptorOfMethodCalled

--- pkg/grpc-middleware/monitor/prometheus/metric.go
+++ pkg/grpc-middleware/monitor/prometheus/metric.go
@@ -6,11 +6,19 @@ import (
 )
 
 const (
-	MetircLabelMethod   = "method"
-	MetircLabelClientIP = "client_ip"
+	MetircLabelMethod      = "method"
+	MetircLabelClientIP    = "client_ip"
+	MetircLabelCodeMessage = "code_message"
 )
 
 var (
+	calledTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "method_called_total",
+		Help: "the total number of method called.",
+	},
+		[]string{MetircLabelMethod, MetircLabelClientIP, MetircLabelCodeMessage},
+	)
+
 	cost = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "method_cost_duration_milliseconds",
