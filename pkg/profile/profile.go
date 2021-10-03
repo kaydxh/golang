bcf950606e056619cc993190c51b2b9918e6dd9e
kayxhding <kayxhding@tencent.com>
2021-10-03 18:37:09 +0800

    feat(profile): generate profile dir, like /profilepath/profile20211003182337.100

--- pkg/profile/profile.go
+++ pkg/profile/profile.go
@@ -1,7 +1,11 @@
 package profile
 
 import (
+	"path/filepath"
 	"strings"
+	"time"
+
+	time_ "github.com/kaydxh/golang/go/time"
 
 	os_ "github.com/kaydxh/golang/go/os"
 	"github.com/pkg/profile"
@@ -17,8 +21,14 @@ func (p *nop) Stop() {
 func StartWithEnv() interface {
 	Stop()
 } {
+
 	profileEnv := os_.GetEnvAsStringOrFallback("PROFILING", "")
-	return start(profileEnv)
+	profilePath := os_.GetEnvAsStringOrFallback("PROFILEPATH", "")
+	if profilePath != "" {
+		profilePath = filepath.Join(profilePath, "profile"+time.Now().Format(time_.TimeMillsFormat))
+	}
+
+	return start(profileEnv, profilePath)
 	//	profiles := strings.Split(profileEnv, ",")
 	/*
 		for _, pf := range profiles {
@@ -27,24 +37,24 @@ func StartWithEnv() interface {
 	*/
 }
 
-func start(pf string) interface {
+func start(pf, dir string) interface {
 	Stop()
 } {
 	switch strings.ToLower(pf) {
 	case "cpu":
-		return profile.Start(profile.CPUProfile, profile.NoShutdownHook)
+		return profile.Start(profile.CPUProfile, profile.NoShutdownHook, profile.ProfilePath(dir))
 	case "mem":
-		return profile.Start(profile.MemProfile, profile.NoShutdownHook)
+		return profile.Start(profile.MemProfile, profile.NoShutdownHook, profile.ProfilePath(dir))
 	case "mutex":
-		return profile.Start(profile.MutexProfile, profile.NoShutdownHook)
+		return profile.Start(profile.MutexProfile, profile.NoShutdownHook, profile.ProfilePath(dir))
 	case "block":
-		return profile.Start(profile.BlockProfile, profile.NoShutdownHook)
+		return profile.Start(profile.BlockProfile, profile.NoShutdownHook, profile.ProfilePath(dir))
 	case "trace":
-		return profile.Start(profile.TraceProfile, profile.NoShutdownHook)
+		return profile.Start(profile.TraceProfile, profile.NoShutdownHook, profile.ProfilePath(dir))
 	case "thread_create":
-		return profile.Start(profile.ThreadcreationProfile, profile.NoShutdownHook)
+		return profile.Start(profile.ThreadcreationProfile, profile.NoShutdownHook, profile.ProfilePath(dir))
 	case "goroutine":
-		return profile.Start(profile.GoroutineProfile, profile.NoShutdownHook)
+		return profile.Start(profile.GoroutineProfile, profile.NoShutdownHook, profile.ProfilePath(dir))
 	default:
 		//do nothing
 		return new(nop)
