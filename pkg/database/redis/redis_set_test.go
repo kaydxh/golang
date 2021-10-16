db7dc2ee795a43649d327aaa8e7fd29a766cc99f
kayxhding <kayxhding@tencent.com>
2021-10-16 20:27:56 +0800

    test(redis): add unit test for set

--- pkg/database/redis/redis_set_test.go
+++ pkg/database/redis/redis_set_test.go
@@ -14,8 +14,14 @@ func TestSAdd(t *testing.T) {
 		Values []string
 	}{
 		{
-			Key:    "set-test-1",
-			Values: []string{"values-set-test-1-1", "values-set-test-1-2"},
+			Key: "set-test-1",
+			Values: []string{
+				"values-set-test-1-1",
+				"values-set-test-1-2",
+				"values-set-test-1-3",
+				"values-set-test-1-4",
+				"values-set-test-1-5",
+			},
 		},
 		{
 			Key:    "set-test-20",
@@ -211,5 +217,215 @@ func TestSRem(t *testing.T) {
 			t.Fatalf("failed to SRem, err: %v", err)
 		}
 		t.Logf("sets: %v, SRem values: %v", testCase.Key, vals)
+	}
+}
+
+// check value is in set
+//need redis server version >= 6.2.0
+func TestSMIsMember(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Key    string
+		Values []string
+	}{
+		{
+			Key:    "set-test-1",
+			Values: []string{"values-set-test-1-2"},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		//err: CROSSSLOT Keys in request don't hash to the same slot
+		vals, err := db.SMIsMember(ctx, testCase.Key, testCase.Values).Result()
+		if err != nil {
+			t.Fatalf("failed to SMIsMember, err: %v", err)
+		}
+		t.Logf("sets: %v, SMIsMember values: %v", testCase.Key, vals)
+	}
+}
+
+//random delete elemnt from set
+func TestSPop(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Key string
+	}{
+		{
+			Key: "set-test-1",
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		val, err := db.SPop(ctx, testCase.Key).Result()
+		if err != nil {
+			t.Fatalf("failed to SPop, err: %v", err)
+		}
+		t.Logf("sets: %v,  SPop value: %v", testCase.Key, val)
+	}
+}
+
+// random get value from set
+func TestSRandMember(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Key string
+	}{
+		{
+			Key: "set-test-1",
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		val, err := db.SRandMember(ctx, testCase.Key).Result()
+		if err != nil {
+			t.Fatalf("failed to SRandMember, err: %v", err)
+		}
+		t.Logf("sets: %v, SRandMember value: %v", testCase.Key, val)
+	}
+}
+
+// random get value from set
+func TestSRandMemberN(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Key   string
+		Count int64
+	}{
+		{
+			Key:   "set-test-1",
+			Count: 2,
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		vals, err := db.SRandMemberN(ctx, testCase.Key, testCase.Count).Result()
+		if err != nil {
+			t.Fatalf("failed to SRandMemberN, err: %v", err)
+		}
+		t.Logf("sets: %v, SRandMemberN values: %v", testCase.Key, vals)
+	}
+}
+
+func TestSUnion(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Keys []string
+	}{
+		{
+			Keys: []string{"set-test-1", "set-test-20"},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		//err: CROSSSLOT Keys in request don't hash to the same slot
+		vals, err := db.SUnion(ctx, testCase.Keys...).Result()
+		if err != nil {
+			t.Fatalf("failed to SUnion, err: %v", err)
+		}
+		t.Logf("sets: %v, SUnion values: %v", testCase.Keys, vals)
+	}
+}
+
+func TestSMove(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Source      string
+		Destination string
+		Value       string
+	}{
+		{
+			Source:      "set-test-1",
+			Destination: "set-test-20",
+			Value:       "values-set-test-1-5",
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		//err: CROSSSLOT Keys in request don't hash to the same slot
+		ok, err := db.SMove(ctx, testCase.Source, testCase.Destination, testCase.Value).Result()
+		if err != nil {
+			t.Fatalf("failed to SUnion, err: %v", err)
+		}
+		t.Logf("move sets: %v to %v, SUnion values: %v", testCase.Source, testCase.Destination, ok)
+	}
+}
+
+func TestSUnionStore(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		TargetSet string
+		Keys      []string
+	}{
+		{
+			TargetSet: "target-set",
+			Keys:      []string{"set-test-1", "set-test-20"},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		//err: CROSSSLOT Keys in request don't hash to the same slot
+		val, err := db.SUnionStore(ctx, testCase.TargetSet, testCase.Keys...).Result()
+		if err != nil {
+			t.Fatalf("failed to SUnionStore, err: %v", err)
+		}
+		t.Logf("sets: %v, SUnionStore values: %v", testCase.Keys, val)
+	}
+}
+
+func TestSScan(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Key    string
+		Cursor uint64
+		Match  string
+		Count  int64
+	}{
+		{
+			Key:    "set-test-1",
+			Cursor: 0,
+			Match:  "*set*",
+			Count:  2,
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		//err: CROSSSLOT Keys in request don't hash to the same slot
+		keys, cursor, err := db.SScan(ctx, testCase.Key, testCase.Cursor, testCase.Match, testCase.Count).Result()
+		if err != nil {
+			t.Fatalf("failed to SScan, err: %v", err)
+		}
+		t.Logf("sets: %v,SScan keys: %v, cursor: %v", testCase.Key, keys, cursor)
 	}
 }
