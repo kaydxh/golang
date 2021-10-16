8e329ff4549f3fbf612c2e92dedea621de9677e8
kayxhding <kayxhding@tencent.com>
2021-10-16 12:07:26 +0800

    test(redis): test for set

--- pkg/database/redis/redis_set_test.go
+++ pkg/database/redis/redis_set_test.go
@@ -85,3 +85,104 @@ func TestSDiff(t *testing.T) {
 		t.Logf("sets: %v, diff values: %v", testCase.Keys, vals)
 	}
 }
+
+func TestSDiffStore(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		TargetSet string
+		Keys      []string
+	}{
+		{
+			TargetSet: "target-set",
+			Keys:      []string{"set-test-1", "set-test-20"},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		//err: CROSSSLOT Keys in request don't hash to the same slot
+		vals, err := db.SDiffStore(ctx, testCase.TargetSet, testCase.Keys...).Result()
+		if err != nil {
+			t.Fatalf("failed to SDiffStore, err: %v", err)
+		}
+		t.Logf("sets: %v,  SDiffStore values: %v", testCase.Keys, vals)
+	}
+}
+
+func TestSInter(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Keys []string
+	}{
+		{
+			Keys: []string{"set-test-1", "set-test-20"},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		vals, err := db.SInter(ctx, testCase.Keys...).Result()
+		if err != nil {
+			t.Fatalf("failed to SDiffStore, err: %v", err)
+		}
+		t.Logf("sets: %v, SInter values: %v", testCase.Keys, vals)
+	}
+}
+
+func TestSMembers(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		Key string
+	}{
+		{
+			Key: "set-test-1",
+		},
+		{
+			Key: "set-test-20",
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		vals, err := db.SMembers(ctx, testCase.Key).Result()
+		if err != nil {
+			t.Fatalf("failed to SMembers, err: %v", err)
+		}
+		t.Logf("sets: %v,  SMembers values: %v", testCase.Key, vals)
+	}
+}
+
+func TestSInterStore(t *testing.T) {
+	db := GetDBOrDie()
+
+	testCases := []struct {
+		TargetSet string
+		Keys      []string
+	}{
+		{
+			TargetSet: "target-inter-set",
+			Keys:      []string{"set-test-1", "set-test-20"},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, testCase := range testCases {
+		//err: CROSSSLOT Keys in request don't hash to the same slot
+		vals, err := db.SInterStore(ctx, testCase.TargetSet, testCase.Keys...).Result()
+		if err != nil {
+			t.Fatalf("failed to SInterStore, err: %v", err)
+		}
+		t.Logf("sets: %v, SInterStore values: %v", testCase.Keys, vals)
+	}
+}
