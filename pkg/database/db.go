65ab1c4a208850cc88b5b8c436ef213069bc2874
kaydxh <523520531@qq.com>
2021-10-18 19:47:05 +0800

    refactor(mysql): remove database context

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -1,18 +1,9 @@
 package database
 
 import (
-	"context"
 	"time"
 )
 
 var (
 	ExecuteTimeout = time.Minute
 )
-
-func WithDatabaseExecuteTimeout(ctx context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
-
-	if timeout > 0 {
-		return context.WithTimeout(ctx, timeout)
-	}
-	return ctx, func() {}
-}
