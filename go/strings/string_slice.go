372e4b82be058f2943114d2ef2c29c263302231e
kayxhding <kayxhding@tencent.com>
2021-11-25 14:51:45 +0800

    feat(strings): remove empty

--- go/strings/string_slice.go
+++ go/strings/string_slice.go
@@ -33,3 +33,37 @@ func SliceIntersection(s1 []string, s2 []string) []string {
 	}
 	return ss
 }
+
+func RemoveEmpty(s []string) []string {
+	var ss []string
+	for _, v := range s {
+		if v != "" {
+			ss = append(ss, v)
+		}
+	}
+
+	return ss
+}
+
+/*
+func SliceIntersectionInt(s1 []int, s2 []int) []int {
+	ss1 := set_.NewObject(set_.GenerateArray([...]int(s1...)))
+	for _, s := range s1 {
+		ss1.Insert(s)
+	}
+
+	ss2 := set_.NewObject()
+	for _, s := range s2 {
+		ss2.Insert(s)
+	}
+
+	ss := []string{}
+	for _, v := range ss1.Intersection(ss2).List() {
+		s, ok := v.(string)
+		if ok {
+			ss = append(ss, s)
+		}
+	}
+	return ss
+}
+*/
