a70fbb2bf429d6000ad7038d0d3ed78dc51308f0
kayxhding <kayxhding@tencent.com>
2021-12-02 16:32:22 +0800

    feat(strings): add replaceAll
    n

--- go/strings/strings_test.go
+++ go/strings/strings_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWriteFile(t *testing.T) {
+func TestReplace(t *testing.T) {
 	testCases := []struct {
 		s        string
 		old      string
@@ -34,6 +34,38 @@ func TestWriteFile(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.s, func(t *testing.T) {
 			newStr := strings_.Replace(testCase.s, testCase.old, testCase.news, testCase.n)
+			t.Logf("newStr: %v", newStr)
+			assert.Equal(t, testCase.expected, newStr)
+
+		})
+
+	}
+}
+
+func TestReplaceAll(t *testing.T) {
+	testCases := []struct {
+		s        string
+		old      string
+		news     []string
+		expected string
+	}{
+		{
+			s:        "task_id in (?)",
+			old:      "?",
+			news:     []string{`"a"`},
+			expected: `task_id in ("a")`,
+		},
+		{
+			s:        "task_id in (?, ?, ?, ?, ?)",
+			old:      "?",
+			news:     []string{`"a"`, `"a"`, `"a"`, `"a"`},
+			expected: `task_id in ("a", "a", "a", "a", "a")`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.s, func(t *testing.T) {
+			newStr := strings_.ReplaceAll(testCase.s, testCase.old, testCase.news)
 			t.Logf("newStr: %v", newStr)
 			assert.Equal(t, testCase.expected, newStr)
 
