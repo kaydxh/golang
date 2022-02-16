2a174e860049c957eed0f9312ed7c9e9c93c2c65
kaydxh <523520531@qq.com>
2022-02-16 22:15:59 +0800

    feat(os): genereate go test with gotests tool

--- go/os/env_test.go
+++ go/os/env_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/google/uuid"
 	os_ "github.com/kaydxh/golang/go/os"
 	"github.com/stretchr/testify/assert"
 )
@@ -20,4 +21,83 @@ func TestGetEnvAsStringOrFallback(t *testing.T) {
 	key = "FLOCKER_UNSET_VAR"
 	assert.Equal(expected, os_.GetEnvAsStringOrFallback(key, expected))
 	assert.NotEqual(expected, os_.GetEnvAsStringOrFallback(key, "~"+expected))
+}
+
+//gotests -w  -all go/os/env.go
+//gotests -only GetEnvAsIntOrFallback go/os/env.go
+func TestUUID(t *testing.T) {
+	id := uuid.New().String()
+	t.Logf("uuid: %v ", id)
+}
+
+func TestGetEnvAsIntOrFallback(t *testing.T) {
+	type args struct {
+		key          string
+		defaultValue int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				key:          "FLOCKER_SET_VAR_INT",
+				defaultValue: 0,
+			},
+			want:    0,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := os_.GetEnvAsIntOrFallback(tt.args.key, tt.args.defaultValue)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetEnvAsIntOrFallback() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetEnvAsIntOrFallback() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetEnvAsFloat64OrFallback(t *testing.T) {
+	type args struct {
+		key          string
+		defaultValue float64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    float64
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				key:          "FLOCKER_SET_VAR_FLOAT",
+				defaultValue: 1.0,
+			},
+			want:    1.0,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := os_.GetEnvAsFloat64OrFallback(tt.args.key, tt.args.defaultValue)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetEnvAsFloat64OrFallback() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetEnvAsFloat64OrFallback() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
