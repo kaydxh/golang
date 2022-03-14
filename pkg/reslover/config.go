8ec1cfedea0cdfd958e5afdb09ab361a18d447f4
kaydxh <523520531@qq.com>
2022-03-14 23:34:51 +0800

    feat(reslover): add reslover

---
+++ pkg/reslover/config.go
@@ -0,0 +1,82 @@
+package reslover
+
+import (
+	"context"
+
+	viper_ "github.com/kaydxh/golang/pkg/viper"
+	"github.com/ory/viper"
+	"github.com/sirupsen/logrus"
+)
+
+type Config struct {
+	Proto Reslover
+	opts  struct {
+		// If set, overrides params below
+		viper *viper.Viper
+	}
+}
+
+type completedConfig struct {
+	*Config
+	completeError error
+}
+
+type CompletedConfig struct {
+	// Embed a private pointer that cannot be instantiated outside of this package.
+	*completedConfig
+}
+
+func (c *completedConfig) New(ctx context.Context) (*ResloverService, error) {
+
+	logrus.Infof("Installing Reslover")
+
+	if c.completeError != nil {
+		return nil, c.completeError
+	}
+
+	if !c.Proto.GetEnabled() {
+		return nil, nil
+	}
+
+	rs, err := c.install(ctx)
+	if err != nil {
+		return nil, err
+	}
+	logrus.Infof("Installed Reslover")
+
+	return rs, nil
+}
+
+func (c *completedConfig) install(ctx context.Context) (*ResloverService, error) {
+	resolverInterval := c.Proto.GetResolveInterval().AsDuration()
+	rs := NewResloverService(resolverInterval)
+	return rs, nil
+}
+
+// Complete set default ServerRunOptions.
+func (c *Config) Complete() CompletedConfig {
+	err := c.loadViper()
+	if err != nil {
+		return CompletedConfig{&completedConfig{
+			Config:        c,
+			completeError: err,
+		}}
+	}
+
+	return CompletedConfig{&completedConfig{Config: c}}
+}
+
+func (c *Config) loadViper() error {
+	if c.opts.viper != nil {
+		return viper_.UnmarshalProtoMessageWithJsonPb(c.opts.viper, &c.Proto)
+	}
+
+	return nil
+}
+
+func NewConfig(options ...ConfigOption) *Config {
+	c := &Config{}
+	c.ApplyOptions(options...)
+
+	return c
+}
---
+++ pkg/reslover/resloverquerymap_syncmap.go
@@ -0,0 +1,72 @@
+package reslover
+
+import (
+	"sync" // Used by sync.Map.
+)
+
+// Generate code that will fail if the constants change value.
+func _() {
+	// An "cannot convert ResloverQueryMap literal (type ResloverQueryMap) to type sync.Map" compiler error signifies that the base type have changed.
+	// Re-run the go-syncmap command to generate them again.
+	_ = (sync.Map)(ResloverQueryMap{})
+}
+
+var _nil_ResloverQueryMap_ResloverQuery_value = func() (val ResloverQuery) { return }()
+
+// Load returns the value stored in the map for a key, or nil if no
+// value is present.
+// The ok result indicates whether value was found in the map.
+func (m *ResloverQueryMap) Load(key string) (ResloverQuery, bool) {
+	value, ok := (*sync.Map)(m).Load(key)
+	if value == nil {
+		return _nil_ResloverQueryMap_ResloverQuery_value, ok
+	}
+	return value.(ResloverQuery), ok
+}
+
+// Store sets the value for a key.
+func (m *ResloverQueryMap) Store(key string, value ResloverQuery) {
+	(*sync.Map)(m).Store(key, value)
+}
+
+// LoadOrStore returns the existing value for the key if present.
+// Otherwise, it stores and returns the given value.
+// The loaded result is true if the value was loaded, false if stored.
+func (m *ResloverQueryMap) LoadOrStore(key string, value ResloverQuery) (ResloverQuery, bool) {
+	actual, loaded := (*sync.Map)(m).LoadOrStore(key, value)
+	if actual == nil {
+		return _nil_ResloverQueryMap_ResloverQuery_value, loaded
+	}
+	return actual.(ResloverQuery), loaded
+}
+
+// LoadAndDelete deletes the value for a key, returning the previous value if any.
+// The loaded result reports whether the key was present.
+func (m *ResloverQueryMap) LoadAndDelete(key string) (value ResloverQuery, loaded bool) {
+	actual, loaded := (*sync.Map)(m).LoadAndDelete(key)
+	if actual == nil {
+		return _nil_ResloverQueryMap_ResloverQuery_value, loaded
+	}
+	return actual.(ResloverQuery), loaded
+}
+
+// Delete deletes the value for a key.
+func (m *ResloverQueryMap) Delete(key string) {
+	(*sync.Map)(m).Delete(key)
+}
+
+// Range calls f sequentially for each key and value present in the map.
+// If f returns false, range stops the iteration.
+//
+// Range does not necessarily correspond to any consistent snapshot of the Map's
+// contents: no key will be visited more than once, but if the value for any key
+// is stored or deleted concurrently, Range may reflect any mapping for that key
+// from any point during the Range call.
+//
+// Range may be O(N) with the number of elements in the map even if f returns
+// false after a constant number of calls.
+func (m *ResloverQueryMap) Range(f func(key string, value ResloverQuery) bool) {
+	(*sync.Map)(m).Range(func(key, value interface{}) bool {
+		return f(key.(string), value.(ResloverQuery))
+	})
+}
