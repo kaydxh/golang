a037ac4dba61e20ef124a7f9856127a7374eb1a0
kaydxh <523520531@qq.com>
2022-03-15 11:33:54 +0800

    test(reslover): test pick node

--- pkg/reslover/reslover_test.go
+++ pkg/reslover/reslover_test.go
@@ -2,45 +2,60 @@ package reslover_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/kaydxh/golang/pkg/reslover"
 	reslover_ "github.com/kaydxh/golang/pkg/reslover"
 	viper_ "github.com/kaydxh/golang/pkg/viper"
 )
 
-func TestNew(t *testing.T) {
-	/*
-		viper.SetConfigName("webserver")
-		viper.SetConfigType("yaml")
-		viper.AddConfigPath(".")
-	*/
-	/*
-		viper.SetConfigFile("./webserver.yaml")
-		err := viper.ReadInConfig()
-		if err != nil {
-			t.Errorf("failed to read config err: %v", err)
-			return
-		}
-		subv := viper.Sub("web")
-	*/
-
+func TestNewResloverService(t *testing.T) {
 	cfgFile := "./reslover.yaml"
 	config := reslover_.NewConfig(reslover_.WithViper(viper_.GetViper(cfgFile, "reslover")))
-
-	c := config.Complete()
-	s, err := c.New(context.Background())
+	s, err := config.Complete().New(context.Background())
 	if err != nil {
 		t.Errorf("failed to new config err: %v", err)
 	}
 	s.Run(context.Background())
-	domain := "www.baid.com"
-	rq := reslover_.ResloverQuery{
-		Domain: domain,
+
+	type args struct {
+		consistkey       string
+		resolverInterval time.Duration
+		services         []reslover_.ResloverQuery
 	}
-	rq.SetDefault()
-	s.AddService(rq)
-	time.Sleep(2 * time.Second)
-	node, has := s.PickNode(domain, "1")
-	t.Logf("has: %v, node: %v", has, node)
+	tests := []struct {
+		name string
+		args args
+		want *reslover_.ResloverService
+	}{
+		// TODO: Add test cases.
+		{
+			name: "www.baidu.com",
+			args: args{
+				consistkey:       "1",
+				resolverInterval: 0,
+				services: []reslover_.ResloverQuery{
+					{
+						Domain: "www.baidu.com",
+						Opts: reslover_.ResloverOptions{
+							ResloverType:    reslover_.Reslover_reslover_type_dns,
+							LoadBalanceMode: reslover.Reslover_load_balance_mode_consist,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rs := reslover_.NewResloverService(tt.args.resolverInterval, tt.args.services...)
+			for i := 0; i < 100; i++ {
+				consistkey := fmt.Sprintf("consist-key-%d", i)
+				node, has := rs.PickNode(tt.name, consistkey)
+				t.Logf("pick node: %v, has: %v, consistkey: %v", node, has, consistkey)
+			}
+		})
+	}
 }
