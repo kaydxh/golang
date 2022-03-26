b4f7a41219eefc49ce2336b6a29508341f8128c8
kayxhding <kayxhding@tencent.com>
2022-03-26 18:20:08 +0800

    refactor(middleware): install base middleware for web server

--- pkg/grpc-middleware/monitor/in_out.packet.go
+++ pkg/grpc-middleware/monitor/in_out.packet.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	reflect_ "github.com/kaydxh/golang/go/reflect"
-	"github.com/sirupsen/logrus"
+	logs_ "github.com/kaydxh/golang/pkg/logs"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
@@ -29,15 +29,16 @@ func UnaryServerInterceptorOfInOutPacket() grpc.UnaryServerInterceptor {
 
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 
+		logger := logs_.GetLogger(ctx)
 		if req != nil {
 			//logrus.WithField("request", &JsonpbMarshaller{req.(proto.Message)}).Info("recv")
-			logrus.WithField("request", reflect_.TruncateBytes(proto.Clone(req.(proto.Message)))).Info("recv")
+			logger.WithField("request", reflect_.TruncateBytes(proto.Clone(req.(proto.Message)))).Info("recv")
 		}
 
 		resp, err := handler(ctx, req)
 		if resp != nil {
 			//logrus.WithField("response", &JsonpbMarshaller{resp.(proto.Message)}).Info("send")
-			logrus.WithField("response", reflect_.TruncateBytes(proto.Clone(resp.(proto.Message)))).Info("send")
+			logger.WithField("response", reflect_.TruncateBytes(proto.Clone(resp.(proto.Message)))).Info("send")
 		}
 
 		return resp, err
