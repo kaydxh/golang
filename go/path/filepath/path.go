6e14499a041eacbc70cbb9ab3ed3cf120428b9c3
kaydxh <523520531@qq.com>
2022-04-04 22:12:39 +0800

    feat(path): add join paths

--- go/path/filepath/path.go
+++ go/path/filepath/path.go
@@ -15,3 +15,31 @@ func GetParentRelDir(filePath string) string {
 	dir := path.Dir(filePath)
 	return path.Base(dir)
 }
+
+func LastChar(str string) uint8 {
+	if str == "" {
+		// char '0' means dec 48, 0 means null
+		return 0
+	}
+
+	return str[len(str)-1]
+}
+
+// join paths, keep relativePath suffix
+func JoinPaths(rootPath, relativePath string) (string, error) {
+	absolutePath, err := filepath.Abs(rootPath)
+	if err != nil {
+		return "", err
+	}
+
+	if relativePath == "" {
+		return absolutePath, nil
+	}
+
+	finalPath := path.Join(absolutePath, relativePath)
+	if LastChar(relativePath) == '/' && LastChar(finalPath) != '/' {
+		return finalPath + "/", nil
+	}
+
+	return finalPath, nil
+}
--- go/path/filepath/path_test.go
+++ go/path/filepath/path_test.go
@@ -1,6 +1,8 @@
 package filepath_test
 
 import (
+	"fmt"
+	"path/filepath"
 	"testing"
 
 	filepath_ "github.com/kaydxh/golang/go/path/filepath"
@@ -28,6 +30,67 @@ func TestGetParentRelPath(t *testing.T) {
 			assert.Equal(t, testCase.expected, rel)
 			t.Logf("rel: %v", rel)
 
+		})
+	}
+}
+
+func TestAbsPath(t *testing.T) {
+	testCases := []struct {
+		filePath string
+		expected string
+	}{
+		{
+			filePath: "",
+			expected: "",
+		},
+		{
+			filePath: ".",
+			expected: "",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.filePath, func(t *testing.T) {
+			abs, err := filepath.Abs(testCase.filePath)
+			if err != nil {
+				t.Fatalf("abs err: %v", err)
+			}
+			t.Logf("abs: %v", abs)
+
+		})
+	}
+}
+
+func TestJoinPaths(t *testing.T) {
+	testCases := []struct {
+		absolutePath string
+		relativePath string
+		expected     string
+	}{
+		{
+			absolutePath: "/data",
+			relativePath: "./relative",
+			expected:     "",
+		},
+		{
+			absolutePath: "./data",
+			relativePath: "./relative",
+			expected:     "",
+		},
+		{
+			absolutePath: "./data",
+			relativePath: "./relative/",
+			expected:     "",
+		},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("test-case-%d", i), func(t *testing.T) {
+			fullPath, err := filepath_.JoinPaths(testCase.absolutePath, testCase.relativePath)
+			if err != nil {
+				t.Fatalf("joinPaths err: %v", err)
+			}
+			t.Logf("fullPath : %v", fullPath)
 		})
 	}
 }
