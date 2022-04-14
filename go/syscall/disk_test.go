12f04479379c1e182043a944b5eb5d2add9d7e93
kayxhding <kayxhding@tencent.com>
2022-04-14 17:42:29 +0800

    feat(syscall): disk usage

---
+++ go/syscall/disk_test.go
@@ -0,0 +1,36 @@
+package syscall_test
+
+import (
+	"testing"
+
+	syscall_ "github.com/kaydxh/golang/go/syscall"
+)
+
+func TestDiskUsage(t *testing.T) {
+	testCases := []struct {
+		volumePath string
+		expected   string
+	}{
+		{
+			volumePath: "/",
+			expected:   "",
+		},
+		{
+			volumePath: "/dev",
+			expected:   "",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.volumePath, func(t *testing.T) {
+			du, err := syscall_.NewDiskUsage(testCase.volumePath)
+			if err != nil {
+				t.Errorf("new disk for path[%v] err, got : %s", testCase.volumePath, err)
+				return
+
+			}
+			t.Logf("disk usage: %v", du.Usage())
+
+		})
+	}
+}
