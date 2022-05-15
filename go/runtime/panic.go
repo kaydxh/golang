4448d6f3b603552bd068a56321e3af5283d4900f
kaydxh <523520531@qq.com>
2022-05-15 22:12:25 +0800

    feat(runtime): must panic on non-nil errors

--- go/runtime/panic.go
+++ go/runtime/panic.go
@@ -55,3 +55,10 @@ func RecoverFromPanic(err *error) {
 			stacktrace)
 	}
 }
+
+// Must panics on non-nil errors. Useful to handling programmer level errors
+func Must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
