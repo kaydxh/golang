006833a20f7633ff1a1bbc7f401b2054d9eefb2a
kaydxh <523520531@qq.com>
2022-05-16 21:31:22 +0800

    feat(gocv): add rect

---
+++ pkg/gocv/rect_test.go
@@ -0,0 +1,48 @@
+package gocv_test
+
+import (
+	"fmt"
+	"testing"
+
+	gocv_ "github.com/kaydxh/golang/pkg/gocv"
+)
+
+func TestScale(t *testing.T) {
+	testCases := []struct {
+		X      int
+		Y      int
+		Width  int
+		Height int
+		factor float32
+	}{
+		{
+			X:      100,
+			Y:      100,
+			Width:  100,
+			Height: 100,
+			factor: 1.1,
+		},
+		{
+			X:      100,
+			Y:      100,
+			Width:  100,
+			Height: 100,
+			factor: 0.9,
+		},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			r := gocv_.Rect{
+				X:      testCase.X,
+				Y:      testCase.Y,
+				Width:  testCase.Width,
+				Height: testCase.Height,
+			}
+			r2 := r.Scale(testCase.factor)
+			t.Logf("r2: %v", r2)
+			r3 := r.Intersect(r2)
+			t.Logf("r3: %v", r3)
+		})
+	}
+}
