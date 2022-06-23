f6ceebcc5b2c1568751f6fb12bd1de0f6c81f68a
kaydxh <xiao_hua_ding@sina.cn>
2022-06-23 10:36:50 +0800

    test(opentelemetry): trace test

--- pkg/monitor/opentelemetry/opentelemetry_test.go
+++ pkg/monitor/opentelemetry/opentelemetry_test.go
@@ -8,6 +8,7 @@ import (
 
 	opentelemetry_ "github.com/kaydxh/golang/pkg/monitor/opentelemetry"
 	viper_ "github.com/kaydxh/golang/pkg/viper"
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/global"
@@ -29,7 +30,7 @@ var (
 	funcNameKey        = attribute.Key("function_name")
 )
 
-func TestNew(t *testing.T) {
+func TestMetric(t *testing.T) {
 	cfgFile := "./opentelemetry.yaml"
 	config := opentelemetry_.NewConfig(opentelemetry_.WithViper(viper_.GetViper(cfgFile, "monitor.open_telemetry")))
 
@@ -56,4 +57,39 @@ func metrics(ctx context.Context, n int) {
 	for i := 0; i < n; i++ {
 		funcLoopCounter.Add(ctx, 1, funcNameKV)
 	}
+}
+
+//https://github.com/open-telemetry/opentelemetry-go/blob/main/example/jaeger/main.go
+func TestTrace(t *testing.T) {
+	cfgFile := "./opentelemetry.yaml"
+	config := opentelemetry_.NewConfig(opentelemetry_.WithViper(viper_.GetViper(cfgFile, "monitor.open_telemetry")))
+
+	ctx := context.Background()
+	err := config.Complete().New(ctx)
+	if err != nil {
+		t.Errorf("failed to new config err: %v", err)
+	}
+	fmt.Printf("config: %+v", config.Proto.String())
+
+	trace := otel.GetTracerProvider()
+	tr := trace.Tracer(instrumentationName)
+
+	ctx, span := tr.Start(ctx, "traceFunc")
+	defer span.End()
+
+	for i := 0; i < 2; i++ {
+		doTrace(ctx)
+		time.Sleep(time.Second)
+	}
+
+}
+
+func doTrace(ctx context.Context) {
+	// Use the global TracerProvider.
+	tr := otel.Tracer("component-doTrace")
+	_, span := tr.Start(ctx, "doTrace")
+	span.SetAttributes(attribute.Key("testset").String("value"))
+	defer span.End()
+
+	time.Sleep(200 * time.Millisecond)
 }
