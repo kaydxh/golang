b04469f0714e4da438b8a9f8cc9271d739fe530b
kaydxh <523520531@qq.com>
2022-07-14 11:05:45 +0800

    feat(context): add ExtractIntegerFromContext

--- go/context/context.go
+++ go/context/context.go
@@ -2,6 +2,8 @@ package context
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -11,4 +13,22 @@ func WithTimeout(ctx context.Context, timeout time.Duration) (context.Context, c
 		return context.WithTimeout(ctx, timeout)
 	}
 	return ctx, func() {}
+}
+
+func ExtractIntegerFromContext(ctx context.Context, key string) (int64, error) {
+	v, ok := ctx.Value(key).(string)
+	if !ok {
+		return 0, fmt.Errorf("key[%v] value type is not string", key)
+	}
+
+	number, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("key[%v] value type is not number: %v", key, err)
+	}
+
+	return number, nil
+}
+
+func SetPairContext(ctx context.Context, key, value string) context.Context {
+	return context.WithValue(ctx, key, value)
 }
