21021d13e60d4944293f59e23101bbbfa672c20f
kaydxh <523520531@qq.com>
2022-08-22 11:12:58 +0800

    feat(resolver): add unix resolver

--- go/net/resolver/resolver.go
+++ go/net/resolver/resolver.go
@@ -3,7 +3,7 @@ package resolver
 import (
 	"fmt"
 
-	"golang.org/x/net/context"
+	"context"
 )
 
 // ResolveNowOptions includes additional information for ResolveNow.
@@ -53,20 +53,4 @@ func GetResolver(ctx context.Context, target string, opts ...ResolverBuildOption
 	}
 
 	return builder.Build(targetInfo)
-}
-
-func ResolveOne(ctx context.Context, target string, opts ...ResolveOneOption) (Address, error) {
-	r, err := GetResolver(ctx, target)
-	if err != nil {
-		return Address{}, err
-	}
-	return r.ResolveOne(opts...)
-}
-
-func ResolveAll(ctx context.Context, target string, opts ...ResolveAllOption) ([]Address, error) {
-	r, err := GetResolver(ctx, target)
-	if err != nil {
-		return nil, err
-	}
-	return r.ResolveAll(opts...)
 }
--- go/net/resolver/resolver_test.go
+++ go/net/resolver/resolver_test.go
@@ -8,6 +8,7 @@ import (
 
 	resolver_ "github.com/kaydxh/golang/go/net/resolver"
 	_ "github.com/kaydxh/golang/go/net/resolver/dns"
+	resolve_ "github.com/kaydxh/golang/go/net/resolver/resolve"
 )
 
 func TestResolveOne(t *testing.T) {
@@ -19,11 +20,15 @@ func TestResolveOne(t *testing.T) {
 			target:   "dns:///www.baidu.com",
 			expected: "",
 		},
+		{
+			target:   "dns:///www.google.com",
+			expected: "",
+		},
 	}
 
 	for i, testCase := range testCases {
 		t.Run(fmt.Sprintf("%d-test", i), func(t *testing.T) {
-			addr, err := resolver_.ResolveOne(context.Background(), testCase.target, resolver_.WithPickMode(resolver_.Resolver_pick_mode_first))
+			addr, err := resolve_.ResolveOne(context.Background(), testCase.target, resolver_.WithPickMode(resolver_.Resolver_pick_mode_first))
 			if err != nil {
 				t.Fatalf("failed to resolve target: %v, err: %v", testCase.target, err)
 			}
@@ -41,11 +46,15 @@ func TestResolveAll(t *testing.T) {
 			target:   "dns:///www.baidu.com",
 			expected: "",
 		},
+		{
+			target:   "dns:///www.google.com",
+			expected: "",
+		},
 	}
 
 	for i, testCase := range testCases {
 		t.Run(fmt.Sprintf("%d-test", i), func(t *testing.T) {
-			addrs, err := resolver_.ResolveAll(context.Background(), testCase.target)
+			addrs, err := resolve_.ResolveAll(context.Background(), testCase.target)
 			if err != nil {
 				t.Fatalf("failed to resolve target: %v, err: %v", testCase.target, err)
 			}
