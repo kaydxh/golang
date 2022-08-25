41d84544d17fe2acfba8e011ba94a68f050a004e
kaydxh <523520531@qq.com>
2022-08-25 15:29:38 +0800

    refactor(http): add comment

--- go/net/http/http_client.go
+++ go/net/http/http_client.go
@@ -65,6 +65,24 @@ func NewClient(options ...ClientOption) (*Client, error) {
 	c.ApplyOptions(options...)
 	//	var transport *http.Transport = http.DefaultTransport
 	transport := &http.Transport{
+
+		// ProxyFromEnvironment returns the URL of the proxy to use for a
+		// given request, as indicated by the environment variables
+		// HTTP_PROXY, HTTPS_PROXY and NO_PROXY (or the lowercase versions
+		// thereof). HTTPS_PROXY takes precedence over HTTP_PROXY for https
+		// requests.
+		//
+		// The environment values may be either a complete URL or a
+		// "host[:port]", in which case the "http" scheme is assumed.
+		// The schemes "http", "https", and "socks5" are supported.
+		// An error is returned if the value is a different form.
+		//
+		// A nil URL and nil error are returned if no proxy is defined in the
+		// environment, or a proxy should not be used for the given request,
+		// as defined by NO_PROXY.
+		//
+		// As a special case, if req.URL.Host is "localhost" (with or without
+		// a port number), then a nil URL and nil error will be returned.
 		Proxy: http.ProxyFromEnvironment,
 	}
 	if c.opts.timeout != 0 {
