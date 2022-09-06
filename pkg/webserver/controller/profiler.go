89354d454c5ee4455442bd41fbdfbe45705b1dfb
kaydxh <523520531@qq.com>
2022-09-06 14:10:27 +0800

    feat(webserver): add pprof

--- pkg/webserver/controller/profiler.go
+++ pkg/webserver/controller/profiler.go
@@ -4,27 +4,24 @@ import (
 	"net/http"
 	"runtime"
 
+	_ "net/http/pprof"
+
 	"github.com/gin-gonic/gin"
 	gw_ "github.com/kaydxh/golang/pkg/grpc-gateway"
 )
 
 type Controller struct {
-	EnableProfiling bool
 }
 
-func NewController(enableProfiling bool) *Controller {
-	return &Controller{
-		EnableProfiling: enableProfiling,
-	}
+func NewController() *Controller {
+	return &Controller{}
 }
 
 func (c *Controller) SetRoutes(ginRouter gin.IRouter, grpcRouter *gw_.GRPCGateway) {
-	if c.EnableProfiling {
-		ginRouter.GET("/debug/pprof/*path", c.PProf())
-	}
+	ginRouter.GET("/debug/pprof/*path", c.Profile())
 }
 
-func (c *Controller) PProf() gin.HandlerFunc {
+func (c *Controller) Profile() gin.HandlerFunc {
 	runtime.SetBlockProfileRate(1)
 	runtime.SetMutexProfileFraction(1)
 	return gin.WrapH(http.DefaultServeMux)
