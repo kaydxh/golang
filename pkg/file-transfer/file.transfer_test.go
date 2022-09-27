c07c0f90b9d2841b87049e675d1b85a3dca7ff40
kaydxh <xiao_hua_ding@sina.cn>
2022-09-27 12:35:33 +0800

    refactor(copyright): add copyright

--- pkg/file-transfer/file.transfer_test.go
+++ pkg/file-transfer/file.transfer_test.go
@@ -1,3 +1,24 @@
+/*
+ *Copyright (c) 2022, kaydxh
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ */
 package filetransfer_test
 
 import (
@@ -21,7 +42,7 @@ func TestDownload(t *testing.T) {
 		Url string
 	}{
 		{
-			Url: "http://127.0.0.1:3306",
+			Url: "https://ai-media-1256936300.cos.ap-guangzhou.myqcloud.com/find.sh?q-sign-algorithm=sha1&q-ak=AKIDCDyve81SJuISPkMq0IukLg7tupWyoqCg&q-sign-time=1659955959;8640000001659870000&q-key-time=1659955959;8640000001659870000&q-header-list=&q-url-param-list=&q-signature=5695f17ee30c3cd2d37197c773a445eda8a70c8c",
 		},
 	}
 
--- pkg/webserver/webserver_test.go
+++ pkg/webserver/webserver_test.go
@@ -1,10 +1,33 @@
+/*
+ *Copyright (c) 2022, kaydxh
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ */
 package webserver_test
 
 import (
+	"fmt"
 	"testing"
 
 	"context"
 
+	prototext_ "github.com/kaydxh/golang/pkg/protobuf/prototext"
 	viper_ "github.com/kaydxh/golang/pkg/viper"
 	webserver_ "github.com/kaydxh/golang/pkg/webserver"
 )
@@ -39,4 +62,15 @@ func TestNew(t *testing.T) {
 	}
 
 	prepared.Run(context.Background())
+}
+
+func TestPrintProto(t *testing.T) {
+	config := &webserver_.Web{
+		BindAddress: &webserver_.Web_Net{
+			Host: "192.168.0.1",
+		},
+	}
+	s := prototext_.FormatWithLength(config, 30)
+	//s := prototext.MarshalOptions{}.Format(config)
+	fmt.Printf("%s\n", s)
 }
