828c97f1eaff9c2e08fcb3f20c40119c0322b779
kaydxh <523520531@qq.com>
2022-09-27 17:09:46 +0800

    build(proto.gen): update download url

--- pkg/gocv/cgo/api/openapi-spec/proto.gen.go
+++ pkg/gocv/cgo/api/openapi-spec/proto.gen.go
@@ -22,5 +22,5 @@
 package openapispec
 
 //go:generate mkdir -p ./scripts
-////go:generate bash -c "curl -s -L -o ./scripts/proto-gen.sh https://raw.githubusercontent.com/kaydxh/sea/master/api/scripts/go_proto_gen.sh"
+//go:generate bash -c "curl -s -L -o ./scripts/proto-gen.sh https://raw.githubusercontent.com/kaydxh/sea/master/script/go_proto_gen.sh"
 //go:generate bash scripts/proto-gen.sh --proto_file_path . -I . -I ../../  --third_party_path ../../../../../third_party --with-go
