27bad5afec2d98a72fb41756b32f0e490334da02
kaydxh <xiao_hua_ding@sina.cn>
2023-03-14 19:48:31 +0800

    feat(kafka): init

---
+++ pkg/mq/kafka/kafka_option.go
@@ -0,0 +1,59 @@
+/*
+ *Copyright (c) 2023, kaydxh
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ */
+package kafka
+
+// A MQOption sets options.
+type MQOption interface {
+	apply(*MQ)
+}
+
+// EmptyMQUrlOption does not alter the MQuration. It can be embedded
+// in another structure to build custom options.
+//
+// This API is EXPERIMENTAL.
+type EmptyMQOption struct{}
+
+func (EmptyMQOption) apply(*MQ) {}
+
+// MQOptionFunc wraps a function that modifies MQ into an
+// implementation of the MQOption interface.
+type MQOptionFunc func(*MQ)
+
+func (f MQOptionFunc) apply(do *MQ) {
+	f(do)
+}
+
+// sample code for option, default for nothing to change
+func _MQOptionWithDefault() MQOption {
+	return MQOptionFunc(func(*MQ) {
+		// nothing to change
+	})
+}
+func (o *MQ) ApplyOptions(options ...MQOption) *MQ {
+	for _, opt := range options {
+		if opt == nil {
+			continue
+		}
+		opt.apply(o)
+	}
+	return o
+}
