22a6ecaff85334f5b50cd5dabd79c547a0cee042
kaydxh <xiao_hua_ding@sina.cn>
2023-04-02 11:53:42 +0800

    refactor(taskqueue): sync register

--- pkg/pool/taskqueue/registry.go
+++ pkg/pool/taskqueue/registry.go
@@ -1,13 +1,40 @@
+/*
+ *Copyright (c) 2023, kaydxh
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ */
 package taskqueue
 
+import "sync"
+
 var (
-	m TaskerMap
+	taskMu sync.RWMutex
+	m      TaskerMap
 )
 
 // Register registers the tasker to the tasker map. b.Name will be
 // used as the name registered with this builder.
 //
 func Register(b Tasker) {
+	taskMu.Lock()
+	defer taskMu.Unlock()
+
 	if b == nil {
 		panic("register tasker is nil")
 	}
@@ -23,6 +50,8 @@ func Register(b Tasker) {
 //
 // If no tasker is register with the scheme, nil will be returned.
 func Get(scheme string) Tasker {
+	taskMu.Lock()
+	defer taskMu.Unlock()
 
 	b, has := m.Load(scheme)
 	if !has {
