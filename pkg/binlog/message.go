1a774d0c39b461b022f086c7fc9c3c6b75956145
kaydxh <523520531@qq.com>
2023-04-23 15:12:40 +0800

    refactor(binlog): operate message

---
+++ pkg/binlog/message.go
@@ -0,0 +1,70 @@
+/*
+ *Copyright (c) 2023, kaydxh
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ */
+package binlog
+
+import (
+	"context"
+	"reflect"
+	"sort"
+)
+
+type MessageKey struct {
+	Key     string
+	MsgType MsgType
+	Cols    []string
+	Table   string
+}
+
+func (m MessageKey) Equual(s MessageKey) bool {
+	if m.Key != s.Key {
+		return false
+	}
+
+	if m.MsgType != s.MsgType {
+		return false
+	}
+
+	if m.Table != s.Table {
+		return false
+	}
+
+	sort.Strings(m.Cols)
+	sort.Strings(s.Cols)
+	return reflect.DeepEqual(m.Cols, s.Cols)
+}
+
+type Message struct {
+	Key   []byte
+	Value []byte
+}
+
+type MsgType int32
+
+const (
+	MsgType_Insert MsgType = 0
+	MsgType_Delete MsgType = 1
+	MsgType_Update MsgType = 2
+	MsgType_Get    MsgType = 3
+)
+
+type MessageDecoderFunc func(ctx context.Context, data []byte) (interface{}, error)
+type MessageKeyDecodeFunc func(ctx context.Context, data []byte) (MessageKey, error)
