5b44bf1d61278a580d1d1166376875f931ada5f1
kaydxh <xiao_hua_ding@sina.cn>
2023-05-26 16:59:03 +0800

    test(cvtable): test for score

--- pkg/gocv/cvtable/cvtable_test.go
+++ pkg/gocv/cvtable/cvtable_test.go
@@ -34,3 +34,31 @@ func TestSim(t *testing.T) {
 		})
 	}
 }
+
+func TestScore(t *testing.T) {
+	cvtable, err := cvtable_.NewCVTable("./testdata/cvtable.conf")
+	if err != nil {
+		t.Fatalf("failed to new cvtable, err: %v", err)
+	}
+
+	testCases := []struct {
+		sim      float64
+		expected string
+	}{
+		{
+			sim:      0.9513,
+			expected: "",
+		},
+		{
+			sim:      1.20013,
+			expected: "",
+		},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("case-%v", i), func(t *testing.T) {
+			score := cvtable.Score(testCase.sim)
+			t.Logf("get score %v for sim %v", score, testCase.sim)
+		})
+	}
+}
