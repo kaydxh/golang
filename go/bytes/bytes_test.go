21af7a7322c4cd1446325043b51dc9e25a9b708b
kaydxh <523520531@qq.com>
2024-06-15 15:16:54 +0800

    feat(bytes): any to bytes

---
+++ go/bytes/bytes_test.go
@@ -0,0 +1,54 @@
+package bytes_test
+
+import (
+	"fmt"
+	"testing"
+
+	"unsafe"
+
+	bytes_ "github.com/kaydxh/golang/go/bytes"
+)
+
+func TestToBytes(t *testing.T) {
+	// to bytes
+	type MyStruct struct {
+		X, Y, Z int
+		Name    string
+	}
+
+	testCases := []struct {
+		s        MyStruct
+		expected string
+	}{
+		{
+			s:        MyStruct{1, 20, 30, "foobar"},
+			expected: "",
+		},
+	}
+	const sz = int(unsafe.Sizeof(MyStruct{}))
+	fmt.Printf("MyStruct size: %v\n", int(unsafe.Sizeof(MyStruct{})))
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
+
+			b := bytes_.Encode(testCase.s)
+			fmt.Println(b)
+			fmt.Printf("size of b: %v\n", len(b))
+
+			/*
+				var p []byte = (*(*[sz]byte)(unsafe.Pointer(&testCase.s)))[:]
+				fmt.Println(p)
+				fmt.Printf("size of p: %v\n", len(p))
+			*/
+
+			/*
+				var m = *(*MyStruct)(unsafe.Pointer(&b[0]))
+				fmt.Println(m)
+			*/
+
+			r := bytes_.Decode[MyStruct](b)
+			fmt.Println(r)
+
+		})
+	}
+}
