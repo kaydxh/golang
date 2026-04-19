6fd4846c201d8a5fba82f138a4e8041103e565e1
kaydxh <523520531@qq.com>
2026-04-19 17:55:54 +0800

    feat(script): go_proto_gen.sh 优先从 Go Module 缓存获取 proto 依赖
    - 通过 go list -m 从 Go Module 缓存获取 grpc-gateway v1 路径
    - 三级回退: Go Module -> 本地 third_party -> 报错退出
    - 合并 yaml 配置文件兜底逻辑, 支持同名 yaml 指定 HTTP 路由映射
    - --third_party_path 参数会清空 GRPC_GATEWAY_DIR 强制使用指定路径
    - 添加 protoc 命令打印, 便于排查 proto 生成问题

--- script/go_proto_gen.sh
+++ script/go_proto_gen.sh
@@ -23,7 +23,9 @@ COMMENT
 
 PROTOC_FILE_DIR=
 PROTO_HEADERS=
-# THIRD_PARTY_DIR=$(realpath "${2:-${SCRIPT_PATH}/../../third_party}")
+# 从 Go Module 缓存中获取 grpc-gateway v1 的路径，其 third_party/googleapis 包含 google/api/annotations.proto
+GRPC_GATEWAY_DIR=$(go list -m -f '{{.Dir}}' github.com/grpc-ecosystem/grpc-gateway 2>/dev/null || echo "")
+# 兼容本地 third_party 目录（当 go module 不可用时作为回退）
 THIRD_PARTY_DIR="${SCRIPT_PATH}/third_party"
 WITH_DOC=
 WITH_CPP=
@@ -47,6 +49,7 @@ function getopts() {
             ;;
        --third_party_path)
            THIRD_PARTY_DIR=$(realpath "$2")
+           GRPC_GATEWAY_DIR=""
             shift
             ;;
        --with-doc)
@@ -98,11 +101,16 @@ done
 
 
 echo "==> Generating proto..."
-#proto_headers="-I ${SCRIPT_PATH}/../../third_party"
-#proto_headers="-I .. -I ${THIRD_PARTY_DIR}"
 # "-I ." need behind PROTO_HEADERS, or remove it
 proto_headers="${PROTO_HEADERS} -I `pwd`"
-proto_headers="${proto_headers} -I ${THIRD_PARTY_DIR}/github.com/grpc-ecosystem/grpc-gateway"
+# 优先从 Go Module 缓存获取 google/api proto 依赖，无需在项目内维护 third_party 副本
+if [[ -n "${GRPC_GATEWAY_DIR}" ]]; then
+  proto_headers="${proto_headers} -I ${GRPC_GATEWAY_DIR}/third_party/googleapis"
+elif [[ -d "${THIRD_PARTY_DIR}/github.com/grpc-ecosystem/grpc-gateway" ]]; then
+  proto_headers="${proto_headers} -I ${THIRD_PARTY_DIR}/github.com/grpc-ecosystem/grpc-gateway"
+else
+  die "无法找到 google/api proto 依赖，请确保 go.mod 中包含 github.com/grpc-ecosystem/grpc-gateway 或通过 --third_party_path 指定"
+fi
 source_relative_option="paths=source_relative:."
 go_opt_option=""
 go_out_option=""
@@ -113,9 +121,10 @@ doc_out_option=""
 cpp_option=""
 cpp_out_option=""
 cpp_grpc_option=""
-grpc_gateway_option=""
+# HTTP 路由优先从 proto 里的 google.api.http 注解读取，同时支持 yaml 配置文件兜底。
 grpc_gateway_out_option="--grpc-gateway_out=logtostderr=true"
 grpc_gateway_delete_option="--grpc-gateway_opt=allow_delete_body=true"
+grpc_gateway_option=""
 
 for proto in $(find ${PROTOC_FILE_DIR} -type f -name '*.proto' -print0 | xargs -0); do
   echo "Generating ${proto}"
@@ -126,9 +135,12 @@ for proto in $(find ${PROTOC_FILE_DIR} -type f -name '*.proto' -print0 | xargs -
   grpc_api_yaml_option=""
   grpc_gateway_option=""
 
+  # 如果存在同名 yaml 配置文件，则使用 grpc_api_configuration 指定 HTTP 路由映射
   if [[ -f "${api_conf_yaml}" ]];then
     grpc_api_yaml_option="grpc_api_configuration=${api_conf_yaml},${source_relative_option}"
     grpc_gateway_option="${grpc_gateway_out_option},${grpc_api_yaml_option} ${grpc_gateway_delete_option}"
+  else
+    grpc_gateway_option="${grpc_gateway_out_option},${source_relative_option} ${grpc_gateway_delete_option}"
   fi
 
   if [[ "${WITH_DOC}" -eq 1 ]]; then
@@ -153,6 +165,8 @@ for proto in $(find ${PROTOC_FILE_DIR} -type f -name '*.proto' -print0 | xargs -
     go_grpc_option="--go-grpc_out=${source_relative_option}"
   fi
 
+  # 打印最终 protoc 调用命令，便于排查 proto 生成问题。
+  echo "+ protoc ${proto_headers} ${go_out_option} ${go_tag_option} ${go_opt_option} ${go_grpc_option} ${grpc_gateway_option} ${cpp_out_option} ${cpp_option} ${doc_option} ${doc_out_option} ${cpp_grpc_option} ${proto}"
   protoc ${proto_headers} ${go_out_option} ${go_tag_option} ${go_opt_option} ${go_grpc_option} ${grpc_gateway_option} ${cpp_out_option} ${cpp_option} ${doc_option} ${doc_out_option} ${cpp_grpc_option} "${proto}"
   #protoc -I . ${proto_headers} --go-tag_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. --grpc-gateway_out=logtostderr=true,grpc_api_configuration=${api_conf_yaml},paths=source_relative:. --grpc-gateway_opt=allow_delete_body=true ${f}
 done
